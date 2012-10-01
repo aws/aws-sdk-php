d202e94fb0e8eecdebc38ada0e25b702e39bf5c9
Ryan Parman <ryan@ryanparman.com>
2012-10-01 16:14:24 -0700

    Updated the bootstrap.php file to point to the correct path.

--- build/vanity/bootstrap.php
+++ build/vanity/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 use Vanity\Console\Utilities;
 
-if (!file_exists(dirname(__DIR__) . '/vendor/composer/autoload_namespaces.php')) {
+if (!file_exists(dirname(dirname(__DIR__)) . '/vendor/composer/autoload_namespaces.php')) {
     $formatters = Utilities::formatters();
     echo $formatters->warning->apply(' You must run Composer\'s `install` task before generating documentation. ') . PHP_EOL . PHP_EOL;
     exit(1);
