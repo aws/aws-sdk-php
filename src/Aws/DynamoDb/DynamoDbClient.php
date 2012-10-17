539daf3ba8992c07ea3115b52fbe50e008b1c82b
Michael Dowling <mtdowling@gmail.com>
2012-10-16 18:27:09 -0700

    Cleaning up client docblocks

--- src/Aws/DynamoDb/DynamoDbClient.php
+++ src/Aws/DynamoDb/DynamoDbClient.php
@@ -18,7 +18,6 @@ namespace Aws\DynamoDb;
 
 use Aws\Common\Client\AbstractClient;
 use Aws\Common\Client\ClientBuilder;
-use Aws\Common\Credentials\CredentialsInterface;
 use Aws\Common\Enum\ClientOptions as Options;
 use Aws\Common\Signature\SignatureV4;
 use Aws\Common\Client\BackoffOptionResolver;
@@ -58,40 +57,41 @@ class DynamoDbClient extends AbstractClient
     protected $directory = __DIR__;
 
     /**
-     * Factory method to create a new Amazon DynamoDB client using an array of configuration options.
+     * Factory method to create a new Amazon DynamoDB client using an array of configuration options:
      *
-     * The following array keys and values are available options:
+     * Credential options (`key`, `secret`, and optional `token` OR `credentials` is required)
      *
-     * - Credential options (`key`, `secret`, and optional `token` OR `credentials` is required)
-     *     - key: AWS Access Key ID
-     *     - secret: AWS secret access key
-     *     - credentials: You can optionally provide a custom `Aws\Common\Credentials\CredentialsInterface` object
-     *     - token: Custom AWS security token to use with request authentication
-     *     - token.ttd: UNIX timestamp for when the custom credentials expire
-     *     - credentials.cache: Used to cache credentials when using providers that require HTTP requests. Set the true
-     *           to use the default APC cache or provide a `Guzzle\Cache\CacheAdapterInterface` object.
-     *     - credentials.cache.key: Optional custom cache key to use with the credentials
-     *     - credentials.client: Pass this option to specify a custom `Guzzle\Http\ClientInterface` to use if your
-     *           credentials require a HTTP request (e.g. RefreshableInstanceProfileCredentials)
-     * - Region and Endpoint options (a `region` and optional `scheme` OR a `base_url` is required)
-     *     - region: Region name (e.g. 'us-east-1', 'us-west-1', 'us-west-2', 'eu-west-1', etc...)
-     *     - scheme: URI Scheme of the base URL (e.g. 'https', 'http').
-     *     - base_url: Instead of using a `region` and `scheme`, you can specify a custom base URL for the client
-     *     - endpoint_provider: Optional `Aws\Common\Region\EndpointProviderInterface` used to provide region endpoints
-     * - Generic client options
-     *     - ssl.cert: Set to true to use the bundled CA cert or pass the full path to an SSL certificate bundle. This
-     *           option should be used when you encounter curl error code 60.
-     *     - curl.options: Array of cURL options to apply to every request.
-     *          See http://www.php.net/manual/en/function.curl-setopt.php for a list of available options
-     * - Signature options
-     *     - signature: You can optionally provide a custom signature implementation used to sign requests
-     *     - signature.service: Set to explicitly override the service name used in signatures
-     *     - signature.region:  Set to explicitly override the region name used in signatures
-     * - Exponential backoff options
-     *     - client.backoff.logger: `Guzzle\Log\LogAdapterInterface` object used to log backoff retries. Use
-     *           'debug' to emit PHP warnings when a retry is issued.
-     *     - client.backoff.logger.template: Optional template to use for exponential backoff log messages. See
-     *           `Guzzle\Plugin\Backoff\BackoffLogger` for formatting information.
+     * - key: AWS Access Key ID
+     * - secret: AWS secret access key
+     * - credentials: You can optionally provide a custom `Aws\Common\Credentials\CredentialsInterface` object
+     * - token: Custom AWS security token to use with request authentication
+     * - token.ttd: UNIX timestamp for when the custom credentials expire
+     * - credentials.cache: Used to cache credentials when using providers that require HTTP requests. Set the true
+     *   to use the default APC cache or provide a `Guzzle\Cache\CacheAdapterInterface` object.
+     * - credentials.cache.key: Optional custom cache key to use with the credentials
+     * - credentials.client: Pass this option to specify a custom `Guzzle\Http\ClientInterface` to use if your
+     *   credentials require a HTTP request (e.g. RefreshableInstanceProfileCredentials)
+     *
+     * Region and Endpoint options (a `region` and optional `scheme` OR a `base_url` is required)
+     *
+     * - region: Region name (e.g. 'us-east-1', 'us-west-1', 'us-west-2', 'eu-west-1', etc...)
+     * - scheme: URI Scheme of the base URL (e.g. 'https', 'http').
+     * - base_url: Instead of using a `region` and `scheme`, you can specify a custom base URL for the client
+     * - endpoint_provider: Optional `Aws\Common\Region\EndpointProviderInterface` used to provide region endpoints
+     *
+     * Generic client options
+     *
+     * - ssl.cert: Set to true to use the bundled CA cert or pass the full path to an SSL certificate bundle. This
+     *   option should be used when you encounter curl error code 60.
+     * - curl.options: Array of cURL options to apply to every request.
+     *   See http://www.php.net/manual/en/function.curl-setopt.php for a list of available options
+     * - signature: You can optionally provide a custom signature implementation used to sign requests
+     * - signature.service: Set to explicitly override the service name used in signatures
+     * - signature.region:  Set to explicitly override the region name used in signatures
+     * - client.backoff.logger: `Guzzle\Log\LogAdapterInterface` object used to log backoff retries. Use
+     *   'debug' to emit PHP warnings when a retry is issued.
+     * - client.backoff.logger.template: Optional template to use for exponential backoff log messages. See
+     *   `Guzzle\Plugin\Backoff\BackoffLogger` for formatting information.
      *
      * @param array|Collection $config Client configuration data
      *
