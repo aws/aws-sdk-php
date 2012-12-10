a4a27347f7f4c8be2befdffe78ae7914044b4527
Jeremy Lindblom <jeremeamia@gmail.com>
2012-12-10 13:24:37 -0800

    Updated CHANGELOG and version number for tagging

--- src/Aws/Common/Aws.php
+++ src/Aws/Common/Aws.php
@@ -27,7 +27,7 @@ class Aws extends ServiceBuilderLoader
     /**
      * @var string Current version of the SDK
      */
-    const VERSION = '2.0.1';
+    const VERSION = '2.0.2';
 
     /**
      * Create a new service locator for the AWS SDK
