7c5977b6cb93e52bb5cbc98b205c0793a9308bd7
Michael Dowling <mtdowling@gmail.com>
2012-12-26 08:49:08 -0800

    Adding restore as a signable sub resource for S3

--- src/Aws/S3/S3Signature.php
+++ src/Aws/S3/S3Signature.php
@@ -37,7 +37,7 @@ class S3Signature implements S3SignatureInterface
         'uploads', 'versionId', 'versioning', 'versions', 'website',
         'response-cache-control', 'response-content-disposition',
         'response-content-encoding', 'response-content-language',
-        'response-content-type', 'response-expires', 'tagging', 'cors'
+        'response-content-type', 'response-expires', 'restore', 'tagging', 'cors'
     );
 
     /**
