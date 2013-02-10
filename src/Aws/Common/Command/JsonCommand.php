47270c49277b80efbd100abf585dc895606dce51
Michael Dowling <mtdowling@gmail.com>
2013-02-09 18:25:57 -0800

    Not using streaming when sending JSON commands to avoid curl error 65. Closes #42

--- src/Aws/Common/Command/JsonCommand.php
+++ src/Aws/Common/Command/JsonCommand.php
@@ -17,6 +17,7 @@
 namespace Aws\Common\Command;
 
 use Guzzle\Service\Command\OperationCommand;
+use Guzzle\Http\Curl\CurlHandle;
 
 /**
  * Adds AWS JSON body functionality to dynamically generated HTTP requests
@@ -37,5 +38,10 @@ class JsonCommand extends OperationCommand
 
         // Never send the Expect header when interacting with a JSON query service
         $this->request->removeHeader('Expect');
+
+        // Always send JSON requests as a raw string rather than using streams to avoid issues with
+        // cURL error code 65: "necessary data rewind wasn't possible".
+        // This could be removed after PHP addresses https://bugs.php.net/bug.php?id=47204
+        $this->request->getCurlOptions()->set(CurlHandle::BODY_AS_STRING, true);
     }
 }
