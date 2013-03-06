91ca459f11c43af71a33abb8649f2d6381185fc2
Michael Dowling <mtdowling@gmail.com>
2013-03-06 11:26:11 -0800

    Removing fork banner and adding button. Adding search sidebar.

--- docs/conf.py
+++ docs/conf.py
@@ -149,8 +149,7 @@ html_static_path = ['_static']
 
 # Custom sidebar templates, maps document names to template names.
 html_sidebars = {
-    '**':       ['sidebarlogo.html', 'localtoc.html', 'relations.html',
-                 'sourcelink.html', 'searchbox.html']
+    '**':       ['sidebarlogo.html', 'localtoc.html', 'relations.html', 'searchbox.html']
 }
 
 # Additional templates that should be rendered to pages, maps page names to
