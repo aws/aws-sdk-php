98712ced24d49aab341d569eb1c601df5c4672a7
Michael Dowling <mtdowling@gmail.com>
2013-07-08 15:19:38 -0700

    Removing unused Vanity stubs (for now at least)

--- build/vanity/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Vanity\Console\Utilities;
-
-// Assist PSR-0 autoloading
-if (!file_exists(dirname(dirname(__DIR__)) . '/vendor/composer/autoload_namespaces.php')) {
-    $formatters = Utilities::formatters();
-    echo $formatters->warning->apply(' You must run Composer\'s `install` task before generating documentation. ') . PHP_EOL . PHP_EOL;
-    exit(1);
-}
