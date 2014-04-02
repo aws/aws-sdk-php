a55873fba806b7ec33d06a4cf03caf19fca35e7c
Michael Dowling <mtdowling@gmail.com>
2014-04-02 14:29:06 -0700

    Adding signature subscriber tests, and updating the error subscriber to parse errors

--- src/Subscriber/Error.php
+++ src/Subscriber/Error.php
@@ -1,35 +1,25 @@
 <?php
-/**
- * Copyright 2010-2013 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- * http://aws.amazon.com/apache2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
-
 namespace Aws\Subscriber;
 
 use GuzzleHttp\Command\Event\CommandErrorEvent;
 use GuzzleHttp\Event\SubscriberInterface;
 
+/**
+ * Listens for command errors and adds the parsed error information to the
+ * command.
+ */
 class Error implements SubscriberInterface
 {
     /** @var callable */
-    private $factory;
+    private $parser;
 
     /**
-     * @param callable $exceptionFactory Factory method that creates exceptions
+     * @param callable $parser AWS error parser that accepts a Response and
+     *                         returns an associative array of error data.
      */
-    public function __construct($exceptionFactory)
+    public function __construct(callable $parser)
     {
-        $this->factory = $exceptionFactory;
+        $this->parser = $parser;
     }
 
     public function getEvents()
@@ -39,6 +29,13 @@ class Error implements SubscriberInterface
 
     public function onError(CommandErrorEvent $event)
     {
+        $response = $event->getRequestErrorEvent()->getResponse();
 
+        // Don't update networking errors
+        if (!$response) {
+            return;
+        }
+
+        $event['error'] = call_user_func($this->parser, $response);
     }
 }
