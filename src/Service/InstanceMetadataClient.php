d85b0ec1e19c3fd5a095b1e2a4611cadfe826ce2
Michael Dowling <mtdowling@gmail.com>
2014-05-07 13:24:12 -0700

    Adding missing instance metadata function and tests

--- src/Service/InstanceMetadataClient.php
+++ src/Service/InstanceMetadataClient.php
@@ -1,6 +1,7 @@
 <?php
 namespace Aws\Service;
 
+use Aws\Waiter\Waiter;
 use GuzzleHttp\Client;
 use GuzzleHttp\ClientInterface;
 
@@ -9,6 +10,11 @@ use GuzzleHttp\ClientInterface;
  */
 class InstanceMetadataClient
 {
+    const URL = 'http://169.254.169.254/latest/';
+
+    /** @var \GuzzleHttp\Client */
+    private $client;
+
     /**
      * @param ClientInterface $client Optionally provide a client to customize
      *                                settings like timeouts.
@@ -16,7 +22,7 @@ class InstanceMetadataClient
     public function __construct(ClientInterface $client = null)
     {
         $this->client = $client ?: new Client([
-            'base_url' => 'http://169.254.169.254/latest/',
+            'base_url' => self::URL,
             'defaults' => [
                 'connect_timeout' => 5,
                 'timeout'         => 10
@@ -27,11 +33,26 @@ class InstanceMetadataClient
     /**
      * Wait until the instance metadata server is responding to requests.
      *
-     * @param int $maxWait Maximum number of seconds to wait
+     * @param int $maxWait Approximate maximum number of seconds to wait
      */
     public function waitUntilRunning($maxWait = 120)
     {
-        // @todo: Implement
+        // Calculate the approximate amount of max retries.
+        $maxAttempts = max(1, (max(1, $maxWait) / 11));
+
+        $waiter = new Waiter(function () {
+            try {
+                $this->client->get('', [
+                    'timeout'         => 10,
+                    'connect_timeout' => 10
+                ]);
+                return true;
+            } catch (\Exception $e) {
+                return false;
+            }
+        }, ['interval' => 0, 'max_attempts' => $maxAttempts]);
+
+        $waiter->wait();
     }
 
     /**
