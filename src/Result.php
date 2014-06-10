3e0b04b54783fa67eff7518a50f0c7e4be795f4d
Michael Dowling <mtdowling@gmail.com>
2014-06-10 13:49:25 -0700

    Model docs string tweaks and proxying to offsetGet

--- src/Result.php
+++ src/Result.php
@@ -5,24 +5,36 @@ use GuzzleHttp\Command\Model;
 use JmesPath;
 
 /**
- * Aws-specific model class representing the result of an API operation
+ * AWS-specific model class representing the result of an API operation
  */
 class Result extends Model
 {
+    /**
+     * Returns the result of executing a JMESPath expression on the contents
+     * of the Result model.
+     *
+     *     $result = $client->execute($command);
+     *     $jpResult = $result->search('foo.*.bar[?baz > `10`]');
+     *
+     * @param string $expression JMESPath expression to execute
+     *
+     * @return mixed Returns the result of the JMESPath expression.
+     * @link http://jmespath.readthedocs.org/en/latest/ JMESPath documentation
+     */
     public function search($expression)
     {
         return JmesPath\search($expression, $this->toArray());
     }
 
     /**
-     * Get a specific key value.
+     * Get a specific key value from the result model.
      *
      * @param string $key Key to retrieve.
      *
-     * @return mixed|null Value of the key or NULL
+     * @return mixed|null Value of the key or NULL if not found.
      */
     public function get($key)
     {
-        return isset($this->data[$key]) ? $this->data[$key] : null;
+        return $this[$key];
     }
 }
