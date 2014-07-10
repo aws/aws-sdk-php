c761d1abab93d22f5ee7f693cdf06c93ab777053
Michael Dowling <mtdowling@gmail.com>
2014-07-10 13:53:04 -0700

    Ignoring the vendor folder when building the docs

--- docs/conf.py
+++ docs/conf.py
@@ -77,7 +77,7 @@ release = version
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', 'vendor']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
