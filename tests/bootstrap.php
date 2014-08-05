fe70a5252814d2bcf189fd64361e1ae9e12bc67c
Michael Dowling <mtdowling@gmail.com>
2014-08-05 16:31:50 -0700

    Clearing JMESPath cache when testing just to be safe

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,3 +6,7 @@ date_default_timezone_set('UTC');
 // Include the composer autoloader
 $loader = require __DIR__ . '/../vendor/autoload.php';
 $loader->addPsr4('Aws\\Test\\', __DIR__);
+
+// Clear our any JMESPath cache if necessary (e.g., COMPILE_DIR is enabled)
+$runtime = JmesPath\Env::createRuntime();
+$runtime->clearCache();
