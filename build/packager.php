af60cdbc21fbe972926ecbcc20030dac36c74623
Jeremy Lindblom <jeremeamia@gmail.com>
2014-09-17 09:02:42 -0700

    One more fix to the Burgomaster packager file.

--- build/packager.php
+++ build/packager.php
@@ -4,6 +4,7 @@ require __DIR__ . '/artifacts/Burgomaster.php';
 $stageDirectory = __DIR__ . '/artifacts/staging';
 $projectRoot = __DIR__ . '/../';
 $burgomaster = new \Burgomaster($stageDirectory, $projectRoot);
+$autoloaderFilename = 'aws-autoloader.php';
 
 $metaFiles = ['README.md', 'LICENSE.md', 'NOTICE.md', 'CHANGELOG.md'];
 foreach ($metaFiles as $file) {
@@ -18,9 +19,9 @@ $burgomaster->recursiveCopy('vendor/psr/log/Psr', 'Psr');
 $burgomaster->recursiveCopy('vendor/monolog/monolog/src/Monolog', 'Monolog');
 $burgomaster->recursiveCopy('vendor/symfony/event-dispatcher/Symfony', 'Symfony');
 
-$burgomaster->createAutoloader(array(), 'aws-autoloader.php');
+$burgomaster->createAutoloader(array(), $autoloaderFilename);
 $burgomaster->createZip(__DIR__ . '/artifacts/aws.zip');
-$burgomaster->createPhar(__DIR__ . '/artifacts/aws.phar');
+$burgomaster->createPhar(__DIR__ . '/artifacts/aws.phar', null, $autoloaderFilename);
 $burgomaster->startSection('test_phar');
 $burgomaster->debug('Phar output: '
     . $burgomaster->exec('php ' . __DIR__ . '/test_phar.php'));
