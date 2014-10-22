bfeba5429749d3d30c253de1d90cdd6442eb0f77
Michael Dowling <mtdowling@gmail.com>
2014-10-22 14:59:47 -0700

    Prettifying PHP file output

--- build/api.php
+++ build/api.php
@@ -27,11 +27,17 @@ function copyJson($file) {
     $phpFile = __DIR__ . '/../src/Common/Resources/api/'
         . str_replace('.json', '.php', basename($file));
     $json = json_decode(file_get_contents($file), true);
-    $script = "<?php\nreturn " . var_export($json, true) . ";\n";
+    $script = "<?php return " . var_export($json, true) . ";\n";
     // Convert "array()" to "[]"
     $script = str_replace('array (', '[', $script);
     $script = str_replace(')', ']', $script);
     // Removing trailing whitespace
     $script = preg_replace('/\s+$/m', '', $script);
+    // Move arrays to the same line
+    $script = preg_replace('/=>\s*\n\s*\[/', '=> [', $script);
+    // Get rid of numbered array indexes
+    $script = preg_replace('/(\s*)(\d+ => )/', '$1', $script);
+    // Adding trailing new line
+    $script .= "\n";
     file_put_contents($phpFile, $script);
 }
