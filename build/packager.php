cc6ca3c7f01cc62b5c3b942b131ceae111b02de7
Michael Dowling <mtdowling@gmail.com>
2015-01-14 17:43:40 -0800

    Updating packaging build step

--- build/packager.php
+++ build/packager.php
@@ -12,45 +12,7 @@ foreach ($metaFiles as $file) {
     $burgomaster->deepCopy($file, $file);
 }
 
-// If a service name has been provided as a parameter, we'll build packages for
-// that service only.
-if (isset($argv[1])) {
-    // Acquire data from the Aws\Sdk class via reflection.
-    $sdk = new ReflectionClass('Aws\\Sdk');
-    $aliases = $sdk->getProperty('aliases');
-    $aliases->setAccessible(true);
-    $aliases = $aliases->getValue(null);
-    $services = $sdk->getProperty('services');
-    $services->setAccessible(true);
-    $services = $services->getValue(null);
-
-    // Determine the service name and namespace, which will be the package name.
-    $namespace = $argv[1];
-    $service = strtolower($argv[1]);
-    $service = isset($aliases[$service]) ? $aliases[$service] : $service;
-    $packageName = $service;
-    if (isset($services[$service])) {
-        $namespace = $services[$service];
-    }
-
-    // Copy the needed files.
-    $burgomaster->recursiveCopy("src/{$namespace}", "Aws/{$namespace}");
-    $burgomaster->recursiveCopy('src/Common', 'Aws/Common', ['php', 'json']);
-
-    // Remove unneeded models.
-    $burgomaster->startSection('remove_models');
-    foreach (glob(realpath($stageDirectory.'/Aws/Common/Resources/api').'/*.php') as $file) {
-        if (strpos(basename($file), $service) !== 0) {
-            $burgomaster->debug("Removing {$file}.");
-            unlink($file);
-        }
-    }
-    $burgomaster->endSection('remove_models');
-} else {
-    $burgomaster->recursiveCopy('src', 'Aws', ['php', 'json']);
-    $packageName = 'aws';
-}
-
+$burgomaster->recursiveCopy('src', 'Aws', ['php', 'json']);
 $burgomaster->recursiveCopy('vendor/guzzlehttp/guzzle/src', 'GuzzleHttp');
 $burgomaster->recursiveCopy('vendor/guzzlehttp/ringphp/src', 'GuzzleHttp/Ring');
 $burgomaster->recursiveCopy('vendor/guzzlehttp/streams/src', 'GuzzleHttp/Stream');
@@ -62,14 +24,15 @@ $burgomaster->recursiveCopy('vendor/mtdowling/jmespath.php/src', 'JmesPath');
 $burgomaster->recursiveCopy('vendor/psr/log/Psr/Log', 'Psr/Log');
 $burgomaster->recursiveCopy('vendor/react/promise/src', 'React/Promise');
 
-$burgomaster->createAutoloader(['React/Promise/functions.php'], $autoloaderFilename);
-$burgomaster->createZip(__DIR__ . "/artifacts/{$packageName}.zip");
-$burgomaster->createPhar(__DIR__ . "/artifacts/{$packageName}.phar", null, $autoloaderFilename);
+$burgomaster->createAutoloader([
+    'React/Promise/functions.php',
+    'JmesPath/JmesPath.php',
+    'Aws/legacy-bootstrap.php'
+], $autoloaderFilename);
 
-$burgomaster->startSection('test_phar');
-if ($packageName === 'aws') {
-    $burgomaster->exec('php ' . __DIR__ . '/test_phar.php');
-} else {
-    $burgomaster->debug('Service-specific builds are not tested.');
-}
+$burgomaster->createZip(__DIR__ . "/artifacts/aws.zip");
+$burgomaster->createPhar(__DIR__ . "/artifacts/aws.phar", null, $autoloaderFilename);
+
+$burgomaster->startSection('test-phar');
+$burgomaster->exec('php ' . __DIR__ . '/test-phar.php');
 $burgomaster->endSection();
---
+++ build/test-phar.php
@@ -0,0 +1,25 @@
+<?php
+require __DIR__ . '/artifacts/aws.phar';
+
+$conf = [
+    'credentials' => ['key' => 'foo', 'secret' => 'bar'],
+    'region'      => 'us-west-2',
+    'version'     => 'latest'
+];
+
+// Normal autoloading of real classes.
+Aws\S3Client::factory($conf);
+
+// Autoloading of legacy class names.
+Aws\DynamoDb\DynamoDbClient::factory($conf);
+
+// JMESPath autolaoder
+\JmesPath\search('foo', ['foo' => 'bar']);
+
+// React autoloader
+if (!function_exists('React\\Promise\\map')) {
+    echo 'React not loaded';
+    exit(1);
+}
+
+echo 'Version=' . Aws\Sdk::VERSION;
