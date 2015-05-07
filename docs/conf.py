58335d9a7caeb91f90b78a7791b30f59f3229a0f
Michael Dowling <mtdowling@gmail.com>
2015-05-07 14:34:28 -0700

    Running docs in a virtualenv because v3 and v2 uses different theme versions

--- docs/conf.py
+++ docs/conf.py
@@ -78,7 +78,7 @@ release = version
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'vendor']
+exclude_patterns = ['_build', 'vendor', 'venv']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
