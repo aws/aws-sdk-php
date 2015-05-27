fcf84dc6b6b61cee38f4c5eaa0b93677e75e87f3
Michael Dowling <mtdowling@gmail.com>
2015-05-26 21:45:13 -0700

    Removing old files

--- src/Aws/Ecs/Exception/EcsException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Aws\Ecs\Exception;
-
-use Aws\Common\Exception\ServiceResponseException;
-
-/**
- * Default service exception class
- */
-class EcsException extends ServiceResponseException {}
