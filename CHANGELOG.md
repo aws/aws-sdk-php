9c030ee5fd8401e26d7f48c4570627ca30b94e81
Michael Dowling <mtdowling@gmail.com>
2015-05-28 17:03:50 -0700

    3.0.1 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## 3.0.1 - 2015-05-28
+
+* `Aws\Lambda` - Added Amazon S3 upload support.
+
 ## 3.0.0 - 2015-05-27
 
 * Asynchronous requests.
--- src/Sdk.php
+++ src/Sdk.php
@@ -53,7 +53,7 @@ namespace Aws;
  */
 class Sdk
 {
-    const VERSION = '3.0.0';
+    const VERSION = '3.0.1';
 
     /** @var array Map of custom lowercase names to service endpoint names. */
     private static $aliases = [
