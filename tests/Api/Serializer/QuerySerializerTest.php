b617be8dd56e6c0dd64265efcc6974ce726b2e16
Jonathan Eskew <eskewj@amazon.com>
2015-06-18 07:57:26 -0700

    Add a test to ensure proper query serialization of flattened lists

--- tests/Api/Serializer/QuerySerializerTest.php
+++ tests/Api/Serializer/QuerySerializerTest.php
@@ -46,4 +46,45 @@ class QuerySerializerTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('http://foo.com', (string) $request->getUri());
         $this->assertEquals('Action=foo&Version=1&baz=', (string) $request->getBody());
     }
+
+    public function testUsesLocationNameToIdentifyMembersOfFlattenedLists()
+    {
+        $service = new Service(
+            [
+                'metadata'=> [
+                    'protocol'   => 'query',
+                    'apiVersion' => '1'
+                ],
+                'operations' => [
+                    'foo' => [
+                        'http' => ['httpMethod' => 'POST'],
+                        'input' => [
+                            'type' => 'structure',
+                            'members' => [
+                                'bars' => [
+                                    'type' => 'list',
+                                    'member' => [
+                                        'type' => 'string',
+                                        'locationName' => 'bar',
+                                    ],
+                                    'flattened' => true,
+                                ],
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+            function () {}
+        );
+
+        $q = new QuerySerializer($service, 'http://foo.com');
+        $cmd = new Command('foo', ['bars' => ['a', 'list', 'of', 'strings']]);
+        $request = $q($cmd);
+        $this->assertEquals('POST', $request->getMethod());
+        $this->assertEquals('http://foo.com', (string) $request->getUri());
+        $this->assertEquals(
+            'Action=foo&Version=1&bar.1=a&bar.2=list&bar.3=of&bar.4=strings',
+            (string) $request->getBody()
+        );
+    }
 }
