e35432514ecb947724effa2b4ce4cc6eed1d3d76
Jonathan Eskew <eskewj@amazon.com>
2015-07-30 13:54:08 -0700

    Try user-specific cache folders

--- src/FileCache.php
+++ src/FileCache.php
@@ -15,22 +15,12 @@ class FileCache implements ClearableCacheInterface
      */
     public function __construct($directory = '')
     {
-        $this->cacheDir = $directory
-            ?: (getenv(self::CACHE_ENV)
-                ?: sys_get_temp_dir() . '/aws-sdk-cache-' . getmyuid()
-            );
         $this->hasOpcache = extension_loaded('Zend OPcache')
-            && function_exists('opcache_is_script_cached');
-
-        if (empty($this->cacheDir)
-            || !$this->initializeDirectory($this->cacheDir)
-        ) {
-            $message = 'Unable to create cache directory: %s. Please make '
-                . 'this directory writable or provide the path to a '
-                . 'writable directory using the AWS_PHP_CACHE_DIR '
-                . 'environment variable.';
-            throw new \RuntimeException(sprintf($message, $directory));
-        }
+            && function_exists('opcache_invalidate');
+        $this->setCacheDir(
+            ($directory ?: getenv(self::CACHE_ENV))
+                ?: sys_get_temp_dir() . '/aws-sdk-cache'
+        );
     }
 
     public function set($key, $data, $ttl = 0)
@@ -91,9 +81,38 @@ class FileCache implements ClearableCacheInterface
         ]);
     }
 
-    private function initializeDirectory($dir)
+    private function setCacheDir($cacheDir)
     {
-        if (is_dir($dir) || (@mkdir($dir, 0755, true) || is_dir($dir))) {
+        if (empty($cacheDir) || !$this->initializeDirectory($cacheDir, 0777)) {
+            $message = 'Unable to create cache directory: %s. Please make '
+                . 'this directory writable or provide the path to a '
+                . 'writable directory using the AWS_PHP_CACHE_DIR '
+                . 'environment variable.';
+            throw new \RuntimeException(sprintf($message, $cacheDir));
+        }
+
+        if (!function_exists('posix_geteuid')) {
+            $userName = posix_getpwuid(posix_geteuid())['name'];
+            $this->cacheDir = "$cacheDir/$userName";
+
+            if (!$this->initializeDirectory($this->cacheDir)
+                || fileowner($this->cacheDir) !== posix_geteuid()
+            ) {
+                throw new \RuntimeException(
+                    "Unable to create user-specific cache directory at "
+                    . "{$this->cacheDir}. Please ensure that you have "
+                    . "permission to write to this location and that no other "
+                    . "user has created a folder there."
+                );
+            }
+        } else {
+            $this->cacheDir = $cacheDir;
+        }
+    }
+
+    private function initializeDirectory($dir, $permissions = 0700)
+    {
+        if (is_dir($dir) || (@mkdir($dir, $permissions, true) || is_dir($dir))) {
             return true;
         }
 
