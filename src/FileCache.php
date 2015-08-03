d8e2a67ae53e8a7e8757d963acea47b53e090a51
Jonathan Eskew <eskewj@amazon.com>
2015-08-03 13:00:48 -0700

    Separate caching from compilation in JsonCompiler

---
+++ src/FileCache.php
@@ -0,0 +1,121 @@
+<?php
+namespace Aws;
+
+class FileCache implements ClearableCacheInterface
+{
+    const CACHE_ENV = 'AWS_PHP_CACHE_DIR';
+
+    /** @var string */
+    private $cacheDir;
+    /** @var bool */
+    private $hasOpcache;
+
+    /**
+     * @param string $directory
+     */
+    public function __construct($directory = '')
+    {
+        $this->hasOpcache = extension_loaded('Zend OPcache')
+            && function_exists('opcache_invalidate');
+        $this->setCacheDir(
+            ($directory ?: getenv(self::CACHE_ENV))
+                ?: sys_get_temp_dir() . '/aws-sdk-cache'
+        );
+    }
+
+    public function set($key, $data, $ttl = 0)
+    {
+        $toSave = ['data' => $data];
+        $path = $this->getCachePath($key);
+        if ($ttl) {
+            $toSave['expiration'] = time() + $ttl;
+        }
+        $toSave = '<?php return ' . var_export($toSave, true) . ';';
+
+        return $this->initializeDirectory(dirname($path))
+            && file_put_contents($path, $toSave, LOCK_EX) > 0;
+    }
+
+    public function remove($key)
+    {
+        $path = $this->getCachePath($key);
+        unlink($path);
+
+        if ($this->hasOpcache) {
+            opcache_invalidate($path);
+        }
+    }
+
+    public function get($key)
+    {
+        $path = $this->getCachePath($key);
+
+        if ($retrieved = @include($path)) {
+            if (isset($retrieved['data']) &&
+                (empty($retrieved['expiration']) || $retrieved['expiration'] > time())
+            ) {
+                return $retrieved['data'];
+            }
+
+            $this->remove($key);
+        }
+    }
+
+    public function purge()
+    {
+        foreach (glob($this->cacheDir . '/**/**/*.cache.php') as $file) {
+            unlink($file);
+        }
+    }
+
+
+    private function getCachePath($key)
+    {
+        $key = md5($key);
+
+        return implode(DIRECTORY_SEPARATOR, [
+            $this->cacheDir,
+            substr($key, 0, 2),
+            substr($key, 2, 2),
+            "$key.cache.php",
+        ]);
+    }
+
+    private function setCacheDir($cacheDir)
+    {
+        if (empty($cacheDir) || !$this->initializeDirectory($cacheDir, 0777)) {
+            $message = 'Unable to create cache directory: %s. Please make '
+                . 'this directory writable or provide the path to a '
+                . 'writable directory using the AWS_PHP_CACHE_DIR '
+                . 'environment variable.';
+            throw new \RuntimeException(sprintf($message, $cacheDir));
+        }
+
+        if (function_exists('posix_geteuid')) {
+            $userName = posix_getpwuid(posix_geteuid())['name'];
+            $this->cacheDir = "$cacheDir/$userName";
+
+            if (!$this->initializeDirectory($this->cacheDir)
+                || fileowner($this->cacheDir) !== posix_geteuid()
+            ) {
+                throw new \RuntimeException(
+                    "Unable to create user-specific cache directory at "
+                    . "{$this->cacheDir}. Please ensure that you have "
+                    . "permission to write to this location and that no other "
+                    . "user has created a folder there."
+                );
+            }
+        } else {
+            $this->cacheDir = $cacheDir;
+        }
+    }
+
+    private function initializeDirectory($dir, $permissions = 0700)
+    {
+        if (is_dir($dir) || (@mkdir($dir, $permissions, true) || is_dir($dir))) {
+            return true;
+        }
+
+        return false;
+    }
+}
--- src/JsonCompiler.php
+++ src/JsonCompiler.php
@@ -2,57 +2,28 @@
 namespace Aws;
 
 /**
- * Loads JSON files and compiles them into PHP files so that they are loaded
- * from PHP's opcode cache.
+ * Loads JSON files and caches them.
  *
  * @internal Please use Aws\load_compiled_json() instead.
  */
 class JsonCompiler
 {
-    const CACHE_ENV = 'AWS_PHP_CACHE_DIR';
-
-    private $cacheDir;
-    private $hasOpcacheCheck;
-    private $useCache;
-    private $stripPath;
+    /** @var ClearableCacheInterface|null */
+    private $cache;
 
     /**
-     * @param bool $useCache Set to false to force the cache to be disabled.
+     * @param ClearableCacheInterface|bool  $cache  The cache to use to store
+     *  compiled JSON (or true to use the default file cache).
      */
-    public function __construct($useCache = true)
+    public function __construct($cache = true)
     {
-        // Because the cache path is normalized to convert windows backslashes
-        // to unix style /, we must do that here as well to ensure that the
-        // relative SDK directory is trimmed from the generated cache file
-        // names.
-        $this->stripPath = str_replace('\\', '/', __DIR__ . DIRECTORY_SEPARATOR);
-        $this->useCache = $useCache && extension_loaded('Zend OPcache');
-        $this->hasOpcacheCheck = $this->useCache
-            && function_exists('opcache_is_script_cached');
-        $this->cacheDir = getenv(self::CACHE_ENV) ?: sys_get_temp_dir();
-        $this->cacheDir .= '/aws-cache-' . str_replace('.', '-', Sdk::VERSION);
-
-        if (!is_dir($this->cacheDir)
-            && !@mkdir($this->cacheDir, 0755, true)
-            && !is_dir($this->cacheDir)) {
-            $message = 'Unable to create cache directory: %s. Please make '
-                . 'this directory writable or provide the path to a '
-                . 'writable directory using the AWS_PHP_CACHE_DIR '
-                . 'environment variable. Note that this cache dir may need '
-                . 'to be cleared when updating the SDK in order to see '
-                . 'updates.';
-            throw new \RuntimeException(sprintf($message, $this->cacheDir));
+        if ($cache instanceof ClearableCacheInterface) {
+            $this->cache = $cache;
+        } elseif (true === $cache) {
+            $dir = getenv(FileCache::CACHE_ENV) ?: sys_get_temp_dir();
+            $dir .= '/aws-cache-' . str_replace('.', '-', Sdk::VERSION);
+            $this->cache = new FileCache($dir);
         }
-    }
-
-    /**
-     * Gets the JSON cache directory.
-     *
-     * @return string
-     */
-    public function getCacheDir()
-    {
-        return $this->cacheDir;
     }
 
     /**
@@ -60,8 +31,8 @@ class JsonCompiler
      */
     public function purge()
     {
-        foreach (glob($this->cacheDir . '/*.json.php') as $file) {
-            unlink($file);
+        if ($this->cache) {
+            $this->cache->purge();
         }
     }
 
@@ -74,86 +45,36 @@ class JsonCompiler
      */
     public function load($path)
     {
-        $real = $this->normalize($path);
-        if (!$this->useCache) {
-            return $this->loadJsonFromFile($path, $real);
+        if (empty($this->cache)) {
+            return $this->loadJsonFromFile($path);
         }
 
-        $cache = str_replace($this->stripPath, '', $real);
-        $cache = str_replace(['\\', '/'], '_', $cache);
-        $cache = "{$this->cacheDir}/{$cache}.php";
-
-        if (($this->hasOpcacheCheck && opcache_is_script_cached($cache))
-            || file_exists($cache)
-        ) {
-            return require $cache;
+        if ($cached = $this->cache->get($path)) {
+            return $cached;
         }
 
-        $data = $this->loadJsonFromFile($path, $real);
-        file_put_contents($cache, "<?php return " . var_export($data, true) . ';');
-        chmod($cache, 0644);
+        $data = $this->loadJsonFromFile($path);
+        $this->cache->set($path, $data);
 
         return $data;
-    }
-
-    /**
-     * Resolve relative paths without using realpath (which causes an
-     * unnecessary fstat). And realpath does not work with phar files!
-     *
-     * @param $path
-     *
-     * @return string
-     */
-    private function normalize($path)
-    {
-        static $skip = ['' => true, '.' => true];
-        $isPhar = substr($path, 0, 7) === 'phar://';
-
-        if ($isPhar) {
-            $path = substr($path, 7);
-        }
-
-        // Normalize path separators
-        $parts = explode('/', str_replace('\\', '/', $path));
-
-        $segments = [];
-        foreach ($parts as $part) {
-            if (isset($skip[$part])) {
-                continue;
-            } elseif ($part === '..') {
-                array_pop($segments);
-            } else {
-                $segments[] = $part;
-            }
-        }
-
-        $resolved = implode('/', $segments);
-
-        // Add a leading slash if necessary.
-        if (isset($parts[0]) && $parts[0] === '') {
-            $resolved = '/' . $resolved;
-        }
-
-        return $isPhar ? 'phar://' . $resolved : $resolved;
     }
 
     /**
      * Loads a JSON file.
      *
      * @param string $path Provided path.
-     * @param string $real Normalized path.
      *
      * @return array
      * @throw \InvalidArgumentException if file does not exist.
      */
-    private function loadJsonFromFile($path, $real)
+    private function loadJsonFromFile($path)
     {
-        if (!file_exists($real)) {
+        if (!file_exists($path)) {
             throw new \InvalidArgumentException(
-                sprintf("File not found: %s, realpath: %s", $path, $real)
+                sprintf("File not found: %s, realpath: %s", $path, realpath($path))
             );
         }
 
-        return json_decode(file_get_contents($real), true);
+        return json_decode(file_get_contents($path), true);
     }
 }
