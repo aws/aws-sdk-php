b67264b7529042d5c7f0a2e5dcb4ed70494a1333
Jonathan Eskew <eskewj@amazon.com>
2015-12-02 16:56:37 -0800

    Add a helper function to generate SMTP password. Resolves #833

---
+++ tests/Ses/SesClientTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Aws\Test\Ses;
+
+use Aws\Credentials\Credentials;
+use Aws\Ses\SesClient;
+
+class SesClientTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCanGenerateSmtpPasswordFromCredentials()
+    {
+        $testCreds = new Credentials(
+            'AKIAIOSFODNN7EXAMPLE',
+            'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+        );
+        // Created using sample code on: http://docs.aws.amazon.com/ses/latest/DeveloperGuide/smtp-credentials.html#smtp-credentials-convert
+        $expectedPassword = 'An60U4ZD3sd4fg+FvXUjayOipTt8LO4rUUmhpdX6ctDy';
+
+        $this->assertSame(
+            $expectedPassword,
+            SesClient::generateSmtpPassword($testCreds)
+        );
+    }
+}
