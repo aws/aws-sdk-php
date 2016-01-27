54408ad00fa1a11f1679d875394ef50929809c3e
Jonathan Eskew <eskewj@amazon.com>
2016-01-27 14:03:25 -0800

    Support legacy CloudFront model

--- Makefile
+++ Makefile
@@ -98,7 +98,10 @@ annotate-clients: clean
 annotate-client-locator: clean
 	php build/annotate-client-locator.php
 
-build: compile-json annotate-clients annotate-client-locator
+build-manifest:
+	php build/build-manifest.php
+
+build: | build-manifest compile-json annotate-clients annotate-client-locator
 
 # Ensures that the TAG variable was passed to the make command
 check-tag:
--- build/build-manifest.php
+++ build/build-manifest.php
@@ -5,6 +5,29 @@
  * with client namespace names.
  */
 
+$compatibleApiVersions = [
+    'apigateway' => [
+        'latest' => [
+            '2015-06-01',
+        ],
+    ],
+    'cloudfront' => [
+        'latest' => [
+            '2016-01-13',
+            '2015-09-17',
+        ],
+        '2015-07-27' => [
+            '2015-04-17',
+            '2014-11-06'
+        ],
+    ],
+    'ec2' => [
+        'latest' => [
+            '2015-04-15',
+        ],
+    ],
+];
+
 // Create a list of possible namespaces so that we can include it in the
 // manifest.
 $possibleNamespaces = [];
@@ -19,30 +42,41 @@ $manifest = [];
 foreach (glob(__DIR__ . '/../src/data/**/**/api-2.json') as $file) {
     $model = json_decode(file_get_contents($file), true);
     $metadata = $model['metadata'] + ['compatibleApiVersions' => []];
+    if (empty($manifest[$metadata['endpointPrefix']])) {
+        // Calculate a namespace for the service.
+        $ns = isset($metadata['serviceAbbreviation'])
+            ? $metadata['serviceAbbreviation']
+            : $metadata['serviceFullName'];
+        $ns = str_replace(['Amazon', 'AWS', 'Beta', '(', ')', ' ', '/'], '', $ns);
 
-    // Calculate a namespace for the service.
-    $ns = isset($metadata['serviceAbbreviation'])
-        ? $metadata['serviceAbbreviation']
-        : $metadata['serviceFullName'];
-    $ns = str_replace(['Amazon', 'AWS', 'Beta', '(', ')', ' ', '/'], '', $ns);
+        if (!isset($possibleNamespaces[strtolower($ns)])) {
+            throw new \Exception('NS not found: ' . $ns);
+        }
 
-    if (!isset($possibleNamespaces[strtolower($ns)])) {
-        throw new \Exception('NS not found: ' . $ns);
+        $ns = $possibleNamespaces[strtolower($ns)];
+
+        $manifest[$metadata['endpointPrefix']] = [
+            'namespace' => $ns,
+            'versions' => [],
+        ];
     }
 
-    $ns = $possibleNamespaces[strtolower($ns)];
+    $manifest[$metadata['endpointPrefix']]['versions'][$metadata['apiVersion']]
+        = $metadata['apiVersion'];
+}
 
-    $manifest[$metadata['endpointPrefix']] = [
-        'namespace' => $ns,
-        'versions'  => [
-            'latest'                => $metadata['apiVersion'],
-            $metadata['apiVersion'] => $metadata['apiVersion']
-        ],
-    ];
-
-    foreach ($metadata['compatibleApiVersions'] as $compatVersion) {
-        $manifest[$metadata['endpointPrefix']]['versions'][$compatVersion] = $metadata['apiVersion'];
+foreach ($manifest as $identifier => &$metadata) {
+    $metadata['versions']['latest'] = max(array_keys($metadata['versions']));
+    foreach ($metadata['versions'] as $name => $version) {
+        if (isset($compatibleApiVersions[$identifier][$name])) {
+            foreach ($compatibleApiVersions[$identifier][$name] as $compatVersion) {
+                $metadata['versions'][$compatVersion]
+                    = $metadata['versions'][$name];
+            }
+        }
     }
+
+    krsort($metadata['versions']);
 }
 
 $data = json_encode($manifest, JSON_PRETTY_PRINT);
