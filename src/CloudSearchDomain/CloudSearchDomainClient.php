b83971185672066bc230d16a1271cb20c3a6e18f
cjyclaire <chejingy@amazon.com>
2016-07-21 10:28:24 -0700

    Use POST instead of GET for CloudSearch Domain Search (#1045)
    * Use POST instead of GET for CloudSearch Domain Search
    
    * fix query string

--- src/CloudSearchDomain/CloudSearchDomainClient.php
+++ src/CloudSearchDomain/CloudSearchDomainClient.php
@@ -2,7 +2,10 @@
 namespace Aws\CloudSearchDomain;
 
 use Aws\AwsClient;
+use Aws\CommandInterface;
 use GuzzleHttp\Psr7\Uri;
+use Psr\Http\Message\RequestInterface;
+use GuzzleHttp\Psr7;
 
 /**
  * This client is used to search and upload documents to an **Amazon CloudSearch** Domain.
@@ -16,6 +19,13 @@ use GuzzleHttp\Psr7\Uri;
  */
 class CloudSearchDomainClient extends AwsClient
 {
+    public function __construct(array $args)
+    {
+        parent::__construct($args);
+        $list = $this->getHandlerList();
+        $list->appendBuild($this->searchByPost(), 'cloudsearchdomain.search_by_POST');
+    }
+
     public static function getArguments()
     {
         $args = parent::getArguments();
@@ -27,5 +37,46 @@ class CloudSearchDomainClient extends AwsClient
         };
 
         return $args;
+    }
+
+    /**
+     * Use POST for search command
+     *
+     * Useful when query string is too long
+     */
+    private function searchByPost()
+    {
+        return static function (callable $handler) {
+            return function (
+                CommandInterface $c,
+                RequestInterface $r = null
+            ) use ($handler) {
+                if ($c->getName() !== 'Search') {
+                    return $handler($c, $r);
+                }
+                return $handler($c, self::convertGetToPost($r));
+            };
+        };
+    }
+
+    /**
+     * Converts default GET request to a POST request
+     *
+     * Avoiding length restriction in query
+     *
+     * @param RequestInterface $r GET request to be converted
+     * @return RequestInterface $req converted POST request
+     */
+    public static function convertGetToPost(RequestInterface $r)
+    {
+        if ($r->getMethod() === 'POST') {
+            return $r;
+        }
+        $query = $r->getUri()->getQuery();
+        $req = $r->withMethod('POST')
+            ->withBody(Psr7\stream_for($query))
+            ->withHeader('Content-Length', strlen($query))
+            ->withHeader('Content-Type', 'application/x-www-form-urlencoded');
+        return $req;
     }
 }
--- tests/CloudSearchDomain/CloudSearchDomainTest.php
+++ tests/CloudSearchDomain/CloudSearchDomainTest.php
@@ -2,6 +2,8 @@
 namespace Aws\Test\CloudSearchDomain;
 
 use Aws\CloudSearchDomain\CloudSearchDomainClient;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7;
 
 /**
  * @covers Aws\CloudSearchDomain\CloudSearchDomainClient
@@ -28,5 +30,20 @@ class CloudSearchDomainTest extends \PHPUnit_Framework_TestCase
             'version'   => 'latest'
         ]);
         $this->assertEquals('us-west-2', $client->getRegion());
+    }
+
+    public function testConvertGetToPost()
+    {
+        $request = new Request(
+            'GET',
+            'http://foo.com?foo=bar',
+            [],
+            ''
+        );
+        $request = CloudSearchDomainClient::convertGetToPost($request);
+        $this->assertEquals('POST', $request->getMethod());
+        $this->assertEquals('application/x-www-form-urlencoded', $request->getHeaderLine('Content-Type'));
+        $this->assertEquals(7, $request->getHeaderLine('Content-Length'));
+        $this->assertEquals('foo=bar', $request->getBody());
     }
 }
