343202b7a2a6375a6457ad6644ef3922afaac79c
4406arthur <4406arthur@gmail.com>
2016-12-02 16:03:35 +0800

    extend v2 version in SignatureProvider

--- src/Signature/SignatureProvider.php
+++ src/Signature/SignatureProvider.php
@@ -109,6 +109,10 @@ class SignatureProvider
     {
         return function ($version, $service, $region) {
             switch ($version) {
+                case 's2' :
+                    return $service === 's3'
+                        ? new S3SignatureV2($service, $region)
+                        : new SignatureV2($service, $region);
                 case 's3v4':
                 case 'v4':
                     return $service === 's3'
