5e6204e9386189e1c86061a069f3b55522f491af
imshashank <agshasha@amazon.com>
2017-02-06 14:55:43 -0800

    Fix tests formatting and code fix

--- build/changelog/ChangelogBuilder.php
+++ build/changelog/ChangelogBuilder.php
@@ -5,126 +5,157 @@ namespace Aws\Build\Changelog;
  * @internal
  */
 
-class ChangelogBuilder 
+class ChangelogBuilder
 {
-  
-  public $newServiceFlag = False;
 
-  public function readChangelog($dir)
-  {
-    $dir = $dir."nextrelease/";
-    $changelogEntries = [];
-    if ( !is_dir($dir) || !$dh = opendir($dir) ) {
-        throw new \Exception("nextrelease directory doesn't exists or is not readable at location $dir");
-    } else {
-      //Ignore any files starting with a (.) dot 
-      $files=preg_grep('/^([^.])/', scandir($dir));
-      if ( empty($files) ) {
-        throw new \Exception("No release notes files found in $dir folder");
-      }
-      foreach ( $files as $file ) {
-        $str = file_get_contents($dir."/".$file);
-        $changelogEntries= array_merge($changelogEntries, $this->cleanJSON(json_decode($str)));
-      }
-      closedir($dh);
+    private $newServiceFlag = false;
+    private $dir;
+
+    public function __construct($dir)
+    {
+        $this->dir = $dir;
     }
-    return $changelogEntries;
-  }
 
-  public function cleanJSON($arr) 
-  {
-    if ( empty($arr) || !is_array($arr) ) {
-      throw new \Exception("Invalid Input", 2);
-    } else {
-        $cleanedJSON=[];
-        foreach ( $arr as $x ) {
-          if ( $x->type == "NEW_SERVICE" ) {
-              $this->newServiceFlag=True;
+    public function getDir()
+    {
+        return $this->dir;
+    }
+
+    public function putDir($dir)
+    {
+        $this->dir = $dir;
+    }
+
+    public function getNewServiceFlag()
+    {
+        return $this->newServiceFlag;
+    }
+
+    public function putNewServiceFlag($flag)
+    {
+        $this->newServiceFlag = $flag;
+    }
+
+    public function readChangelog()
+    {
+        $releaseDir = $this->dir . ".changes/nextrelease/";
+        $changelogEntries = [];
+        if (!is_dir($releaseDir) || !$dh = opendir($releaseDir)) {
+            throw new \Exception("nextrelease directory doesn't exists or is not readable at location $releaseDir");
+        } else {
+            //Ignore any files starting with a (.) dot
+            $files = preg_grep('/^([^.])/', scandir($releaseDir));
+            if (empty($files)) {
+                throw new \Exception("No release notes files found in $releaseDir folder");
             }
-          if ( $x->type != "DOC_UPDATE" ) {
-            array_push($cleanedJSON,$x);
-          }
+            foreach ($files as $file) {
+                $str = file_get_contents($releaseDir . "/" . $file);
+                $changelogEntries = array_merge($changelogEntries, $this->cleanJSON(json_decode($str)));
+            }
+            closedir($dh);
         }
-        return $cleanedJSON;
+        return $changelogEntries;
     }
-  }
 
-  public function createTag($newServiceFlag,$changelogFile)
-  {
-    if ( !file_exists($changelogFile) ) {
-      throw new \Exception("Changelog File Not Found", 2);
+    public function cleanJSON($arr)
+    {
+        if (empty($arr) || !is_array($arr)) {
+            throw new \Exception("Invalid Input", 2);
+        } else {
+            $cleanedJSON = [];
+            foreach ($arr as $x) {
+                if ($x->type == "NEW_SERVICE") {
+                    $this->newServiceFlag = true;
+                }
+                if ($x->type != "DOC_UPDATE") {
+                    array_push($cleanedJSON, $x);
+                }
+            }
+            return $cleanedJSON;
+        }
     }
-    $lines = file($changelogFile);  
-    $TAG = explode(".",explode(" ",$lines[2])[1]);
-    if ( $TAG[0] == "next") {
-      throw new \Exception("Untagged changes exits in CHANGELOG.md", 1);
+
+    public function createTag($changelogFile)
+    {
+        if (!file_exists($changelogFile)) {
+            throw new \Exception("Changelog File Not Found", 2);
+        }
+        $lines = file($changelogFile);
+        $TAG = explode(".", explode(" ", $lines[2])[1]);
+        if ($TAG[0] == "next") {
+            throw new \Exception("Untagged changes exits in CHANGELOG.md", 1);
+        }
+        if ($this->newServiceFlag) {
+            //Minor Version Bump if a newservice is being released
+            ++$TAG[1];
+            $TAG[2] = 0;
+            return implode(".", $TAG);
+        } else {
+            ++$TAG[2];
+            return implode(".", $TAG);
+        }
     }
-    if ( $newServiceFlag ) {
-      //Minor Version Bump if a newservice is being released
-      ++$TAG[1];
-      $TAG[2] = 0;
-      return implode(".",$TAG);
-    } else {
-      ++$TAG[2];
-      return implode(".",$TAG);
+
+    public function createChangelogJson($changelog, $tag, $changesFolder)
+    {
+        if ($changesFolder == "") {
+            $changesFolder = $this->dir;
+        }
+        $fp = fopen($changesFolder . "/" . $tag, 'w');
+        fwrite($fp, json_encode($changelog, JSON_PRETTY_PRINT));
+        fclose($fp);
     }
-  }
 
-  public function createChangelogJson($dir,$CHANGELOG,$TAG)
-  {
-    $fp = fopen($dir . "/" . $TAG, 'w');
-    fwrite($fp, json_encode($CHANGELOG, JSON_PRETTY_PRINT));
-    fclose($fp);
-  }
-
-  public function writeToChangelog($CHANGELOG,$changelogFile)
-  {
-    $NEWCHANGELOG = "## next release\n\n".$CHANGELOG."\n";
-    $lines = file($changelogFile);
-    $lines[2] = $NEWCHANGELOG.$lines[2];
-    file_put_contents($changelogFile, $lines);
-  }
-
-  public function cleanNextReleaseFolder()
-  {
-    $dir = ".changes/nextrelease/";
-    $files = preg_grep('/^([^.])/', scandir($dir));
-    foreach( $files as $file ) {
-      if( is_file($dir.$file) )
-        unlink($dir.$file);
+    public function writeToChangelog($changelog, $changelogFile)
+    {
+        $newChangeLog = "## next release\n\n" . $changelog . "\n";
+        $lines = file($changelogFile);
+        $lines[2] = $newChangeLog . $lines[2];
+        file_put_contents($changelogFile, $lines);
     }
-  }
 
-  public function generateChangelogString($CHANGELOG)
-  {
-    usort($CHANGELOG, function ($a, $b) {
-      return strcmp($a->category, $b->category);
-    });
-    $str = "";
-    foreach ( $CHANGELOG as $log ) {
-      $str .= "* `Aws\\".$log->category."` - ".$log->description."\n";
+    public function cleanNextReleaseFolder()
+    {
+        $nextReleaseDir = ".changes/nextrelease/";
+        $files = preg_grep('/^([^.])/', scandir($nextReleaseDir));
+        foreach ($files as $file) {
+            if (is_file($nextReleaseDir . $file)) {
+                unlink($nextReleaseDir . $file);
+            }
+
+        }
     }
-    return $str;
-  }
 
-  public function buildChangelog()
-  {
-    $dir = ".changes/";
-    $changelogFile = "CHANGELOG.md";
-    $newChangelog = $this->readChangelog($dir);
-    $TAG = $this->createTag($this->newServiceFlag,$changelogFile);
-    putenv("TAG=$TAG");
-    echo "Tag for next release ".$TAG. "\n";
-    $this->createChangelogJson($dir,$newChangelog,$TAG);
-    $ChangelogUpdate=$this->generateChangelogString($newChangelog);
-    echo "$ChangelogUpdate";
-    $this->writeToChangelog($ChangelogUpdate,$changelogFile);
-  }
+    public function generateChangelogString($changelog)
+    {
+        usort($changelog, function ($a, $b) {
+            return strcmp($a->category, $b->category);
+        });
+        $str = "";
+        foreach ($changelog as $log) {
+            $str .= "* `Aws\\" . $log->category . "` - " . $log->description . "\n";
+        }
+        return $str;
+    }
 
-  public function fixEndpointFile()
-  {
-    //Replace string in sdb as PHP doesnt support v2
-    $oldMessage = '"sdb": {
+    public function buildChangelog()
+    {
+        $dir = ".changes/";
+        $changelogFile = "CHANGELOG.md";
+        $newChangelog = $this->readChangelog();
+        $TAG = $this->createTag($changelogFile);
+        putenv("TAG=$TAG");
+        echo "Tag for next release " . $TAG . "\n";
+        $this->createChangelogJson($newChangelog, $TAG, "");
+        $ChangelogUpdate = $this->generateChangelogString($newChangelog);
+        echo "$ChangelogUpdate";
+        $this->writeToChangelog($ChangelogUpdate, $changelogFile);
+    }
+
+    public function fixEndpointFile()
+    {
+        //Replace string in sdb as PHP doesnt support v2
+        $oldMessage = '"sdb": {
               "defaults": {
                 "protocols": [
                   "http",
@@ -135,15 +166,15 @@ class ChangelogBuilder
                 ]
               }';
 
-    $deletedFormat = '"sdb": {
+        $deletedFormat = '"sdb": {
               "defaults": {
                 "protocols": [
                   "http",
                   "https"
                 ]
               }';
-    $str = file_get_contents('src/data/endpoints.json');
-    $str = str_replace("$oldMessage", "$deletedFormat",$str);
-    file_put_contents('src/data/endpoints.json', $str);
-  }
+        $str = file_get_contents('src/data/endpoints.json');
+        $str = str_replace("$oldMessage", "$deletedFormat", $str);
+        file_put_contents('src/data/endpoints.json', $str);
+    }
 }
--- tests/Build/Changelog/ChangelogBuilderTest.php
+++ tests/Build/Changelog/ChangelogBuilderTest.php
@@ -3,139 +3,156 @@ namespace Aws\Test\Build\Changelog;
 
 use Aws\Build\Changelog\ChangelogBuilder;
 
+/**
+ * @covers ChangelogBuilder
+ *
+ */
 class ChangelogBuilderTest extends \PHPUnit_Framework_TestCase
 {
-	/**
-	 * @covers ChangelogBuilder
-	 *
-	 */
 
-	private $RESOURCE_DIR= "tests/Build/Changelog/resources";
+    private $RESOURCE_DIR = "tests/Build/Changelog/resources/";
 
-	private function getChangelogBuilder()
-	{
-		return new ChangelogBuilder();
-	}
+    private function getChangelogBuilder()
+    {
+        return new ChangelogBuilder($this->RESOURCE_DIR);
+    }
 
-	public function testreadChangelogNoDirectory()
-	{
-		$this->setExpectedException(\Exception::class);
-		$obj =$this->getChangelogBuilder();
-		$result = $obj->readChangelog("wrong-folder");
-	}
+    /**
+     * @expectedException \Exception
+     */
+    public function testReadChangelogNoDirectory()
+    {
+        $obj = $this->getChangelogBuilder();
+        $obj->putDir("wrong-folder");
+        $obj->readChangelog();
+    }
 
-	public function testreadChangelogNoReleaseNotes()
-	{
-		$obj = $this->getChangelogBuilder();
-		$this->setExpectedException(\Exception::class);
-		$obj->readChangelog($this->RESOURCE_DIR);
-	}
+    /**
+     * @expectedException \Exception
+     */
+    public function testReadChangelogNoReleaseNotes()
+    {
+        $obj = $this->getChangelogBuilder();
+        $obj->putDir($this->RESOURCE_DIR . "/.changes/");
+        $obj->readChangelog();
+    }
 
-	public function testreadChangelogValid()
-	{
-		$obj = $this->getChangelogBuilder();
-		$result = $obj->readChangelog($this->RESOURCE_DIR . "/.changes/");
-		$this->assertEquals($result[0]->type,"NEW_FEATURE");
-		$this->assertEquals($result[0]->description,"Parse ini files containing comments using #");
-		$this->assertEquals($result[1]->type,"NEW_SERVICE");
-	}
+    public function testReadChangelogValid()
+    {
+        $obj = $this->getChangelogBuilder();
+        $result = $obj->readChangelog();
+        $this->assertEquals($result[0]->type, "NEW_FEATURE");
+        $this->assertEquals($result[0]->description, "Parse ini files containing comments using #");
+        $this->assertEquals($result[1]->type, "NEW_SERVICE");
+    }
 
-	public function testCleanJSONEmptyInput()
-	{
-		$this->setExpectedException(\Exception::class);
-		$obj = $this->getChangelogBuilder();
-		$obj->cleanJSON("");
-	}
+    /**
+     * @expectedException \Exception
+     */
+    public function testCleanJSONEmptyInput()
+    {
+        $obj = $this->getChangelogBuilder();
+        $obj->cleanJSON("");
+    }
 
-	public function testCleanJSONInvalidJSON()
-	{
-		$this->setExpectedException(\Exception::class);
-		$obj = $this->getChangelogBuilder();
-		$obj->cleanJSON("test");
-	}
+    /**
+     * @expectedException \Exception
+     */
+    public function testCleanJSONInvalidJSON()
+    {
+        $obj = $this->getChangelogBuilder();
+        $obj->cleanJSON("test");
+    }
 
-	public function testCleanJSONValidJSON()
-	{
-		$str = file_get_contents($this->RESOURCE_DIR . "/.changes/nextrelease/test.json");
-		$obj = $this->getChangelogBuilder();
-		$obj->newServiceFlag=False;
-		$result=$obj->cleanJSON(json_decode($str));
-		$this->assertEquals($result[0]->description,"Parse ini files containing comments using #");
-		$this->assertEquals($result[1]->type,"NEW_SERVICE");
-	}
+    public function testCleanJSONValidJSON()
+    {
+        $str = file_get_contents($this->RESOURCE_DIR . "/.changes/nextrelease/test.json");
+        $obj = $this->getChangelogBuilder();
+        $obj->putNewServiceFlag(false);
+        $result = $obj->cleanJSON(json_decode($str));
+        $this->assertEquals($result[0]->description, "Parse ini files containing comments using #");
+        $this->assertEquals($result[1]->type, "NEW_SERVICE");
+    }
 
-	public function testcreateTagNoFile()
-	{
-		$this->setExpectedException(\Exception::class);
-		$obj = $this->getChangelogBuilder();
-		$obj->createTag(False,"test");
-	}
+    /**
+     * @expectedException \Exception
+     */
+    public function testCreateTagNoFile()
+    {
+        $obj = $this->getChangelogBuilder();
+        $obj->createTag(false, "test");
+    }
 
-	public function testcreateTagInvalidChangelog()
-	{
-		$this->setExpectedException(\Exception::class);
-		$obj = $this->getChangelogBuilder();
-		$obj->createTag(False,$this->RESOURCE_DIR . "/CHANGELOG-invalid.md");
-	}
+    /**
+     * @expectedException \Exception
+     */
+    public function testCreateTagInvalidChangelog()
+    {
+        $obj = $this->getChangelogBuilder();
+        $obj->createTag( $this->RESOURCE_DIR . "CHANGELOG-invalid.md");
+    }
 
-	public function testcreateTagValid()
-	{
-		$obj = $this->getChangelogBuilder();
-		$result = $obj->createTag(False,$this->RESOURCE_DIR . "/CHANGELOG-valid.md");
-		$this->assertEquals("3.21.7",$result);
-	}
+    public function testCreateTagValid()
+    {
+        $obj = $this->getChangelogBuilder();
+        $result = $obj->createTag($this->RESOURCE_DIR . "/CHANGELOG-valid.md");
+        $this->assertEquals("3.21.7", $result);
+    }
 
-	public function testcreateTagValidServiceVersionBump()
-	{
-		$obj = $this->getChangelogBuilder();
-		$result = $obj->createTAG(True,$this->RESOURCE_DIR . "/CHANGELOG-valid.md");
-		$this->assertEquals("3.22.0",$result);
-	}
+    public function testCreateTagValidServiceVersionBump()
+    {
+        $obj = $this->getChangelogBuilder();
+        $obj->putNewServiceFlag(true);
+        $result = $obj->createTAG($this->RESOURCE_DIR . "CHANGELOG-valid.md");
+        $this->assertEquals("3.22.0", $result);
+    }
 
-	public function testcreateChangelogJsonValid()
-	{
-		if ( file_exists($this->RESOURCE_DIR . "/3.21.7") ) {
-			unlink($this->RESOURCE_DIR . "/3.21.7");
-		}
-		$obj = $this->getChangelogBuilder();
-		$obj->dir = $this->RESOURCE_DIR;
-		$CHANGELOG=$obj->readChangelog($this->RESOURCE_DIR . "/.changes/");
-		$TAG = $obj->createTag(False,$this->RESOURCE_DIR . "/CHANGELOG-valid.md");
-		$obj->createChangelogJson($this->RESOURCE_DIR, $CHANGELOG,$TAG);
-		$this->assertEquals(True, file_exists($this->RESOURCE_DIR . "/3.21.7"));
-		$result = json_decode(file_get_contents($this->RESOURCE_DIR . "/3.21.7"));
-		$this->assertEquals(3,count($result));
-		unlink($this->RESOURCE_DIR . "/3.21.7");
-	}
+    public function testCreateChangelogJsonValid()
+    {
+        if (file_exists($this->RESOURCE_DIR . "/3.21.7")) {
+            unlink($this->RESOURCE_DIR . "/3.21.7");
+        }
+        $tempDir = sys_get_temp_dir();
+        $obj = $this->getChangelogBuilder();
+        $CHANGELOG = $obj->readChangelog();
+        $obj->putNewServiceFlag(false);
+        $TAG = $obj->createTag($this->RESOURCE_DIR . "/CHANGELOG-valid.md");
+        $obj->createChangelogJson($CHANGELOG, $TAG, $tempDir);
+        $this->assertEquals(true, file_exists($tempDir . "/3.21.7"));
+        $result = json_decode(file_get_contents($tempDir . "/3.21.7"));
+        $this->assertEquals(3, count($result));
+        unlink($tempDir . "/3.21.7");
+    }
 
-	public function testwriteToChangelogValid()
-	{
-		file_put_contents($this->RESOURCE_DIR . "/CHANGELOG-tmp.md", "# CHANGELOG \n\n\n\n ");
-		$obj = $this->getChangelogBuilder();
-		$changelogEntries_arr=$obj->readChangelog($this->RESOURCE_DIR . "/.changes/");
-		$changelogEntries = $obj->generateChangelogString($changelogEntries_arr);
-		$obj->writeToChangelog($changelogEntries,$this->RESOURCE_DIR . "/CHANGELOG-tmp.md");
-		$lines = file($this->RESOURCE_DIR . "/CHANGELOG-tmp.md");
-		$this->assertEquals("## next release\n",$lines[2]);
-		$i=4;
-		
-		foreach ( explode( "\n" , $changelogEntries ) as $actualLine ) {
-			$this->assertEquals($actualLine."\n",$lines[$i]);
-			++$i;
-		}
-		unlink($this->RESOURCE_DIR . "/CHANGELOG-tmp.md");
-	}
+    public function testWriteToChangelogValid()
+    {
+        $tempDir = sys_get_temp_dir();
+        file_put_contents($tempDir . "/CHANGELOG-tmp.md", "# CHANGELOG \n\n\n\n ");
+        $obj = $this->getChangelogBuilder();
+        $changelogEntries_arr = $obj->readChangelog();
+        $changelogEntries = $obj->generateChangelogString($changelogEntries_arr);
+        $obj->writeToChangelog($changelogEntries, $tempDir . "/CHANGELOG-tmp.md");
+        $lines = file($tempDir . "/CHANGELOG-tmp.md");
+        $this->assertEquals("## next release\n", $lines[2]);
+        $i = 4;
 
-	public function testgenerateChangelogStringValid()
-	{
-		$obj = $this->getChangelogBuilder();
-		$CHANGELOG=$obj->readChangelog($this->RESOURCE_DIR . "/.changes/");
-		$result = $obj->generateChangelogString($CHANGELOG);
-		$expectedOutput=
-"* `Aws\Ec2` - Added Support to Tag Instance
+        foreach (explode("\n", $changelogEntries) as $actualLine) {
+            $this->assertEquals($actualLine . "\n", $lines[$i]);
+            ++$i;
+        }
+        unlink($tempDir . "/CHANGELOG-tmp.md");
+    }
+
+    public function testGenerateChangelogStringValid()
+    {
+        $obj = $this->getChangelogBuilder();
+        $CHANGELOG = $obj->readChangelog();
+        $result = $obj->generateChangelogString($CHANGELOG);
+        $expectedOutput =
+            "* `Aws\Ec2` - Added Support to Tag Instance
 * `Aws\s3` - Test string placeholder for new service
 * `Aws\util` - Parse ini files containing comments using #
 ";
-		$this->assertEquals($expectedOutput,$result);
-	}
+        $this->assertEquals($expectedOutput, $result);
+    }
 }
