0ea989c99324ec7c728563ea557028a6b296a643
imshashank <agshasha@amazon.com>
2017-02-09 17:06:58 -0800

    Update resources json

--- tests/Build/Changelog/ChangelogBuilderTest.php
+++ tests/Build/Changelog/ChangelogBuilderTest.php
@@ -27,7 +27,7 @@ class ChangelogBuilderTest extends \PHPUnit_Framework_TestCase
      */
     public function testBuildChangelogNoReleaseNotes()
     {
-        $tempDir = sys_get_temp_dir();
+        $tempDir = sys_get_temp_dir() . "/";
         if (!file_exists($tempDir . "/.changes/nextrelease/")) {
             mkdir($tempDir . "/.changes/nextrelease/", 0777, true);
         }
@@ -47,11 +47,11 @@ class ChangelogBuilderTest extends \PHPUnit_Framework_TestCase
 
     public function testBuildChangelogValid()
     {
-        $tempDir = sys_get_temp_dir();
-        file_put_contents($tempDir . "/CHANGELOG.md", "# CHANGELOG \n\n\n\n ");
+        $tempDir = sys_get_temp_dir() . "/";
+        file_put_contents($tempDir . "CHANGELOG.md", "# CHANGELOG \n\n\n\n ");
         $obj = new ChangelogBuilder($this->RESOURCE_DIR, $tempDir, false);
         $obj->buildChangelog();
-        $lines = file($tempDir . '/CHANGELOG.md');
+        $lines = file($tempDir . 'CHANGELOG.md');
         $this->assertEquals("## next release\n", $lines[2]);
         $this->assertEquals("* `Aws\Ec2` - Added Support to Tag Instance\n", $lines[4]);
         $this->assertEquals("* `Aws\s3` - Test string placeholder for new service\n", $lines[5]);
@@ -59,23 +59,23 @@ class ChangelogBuilderTest extends \PHPUnit_Framework_TestCase
         unlink($tempDir . '/CHANGELOG.md');
 
         $this->assertEquals(
-            json_decode(file_get_contents($this->RESOURCE_DIR . "/3.22.0"), true),
-            json_decode(file_get_contents($tempDir . "/3.22.0"), true)
+            json_decode(file_get_contents($this->RESOURCE_DIR . "/release-json-valid"), true),
+            json_decode(file_get_contents($tempDir . "3.22.0"), true)
         );
-        unlink($tempDir . "/3.22.0");
+        unlink($tempDir . "3.22.0");
     }
 
     public function testCleanNextReleaseFolderValid()
     {
-        $tempDir = sys_get_temp_dir();
-        if (!file_exists($tempDir . "/.changes/nextrelease/")) {
-            mkdir($tempDir . "/.changes/nextrelease/", 0777, true);
+        $tempDir = sys_get_temp_dir() . "/";
+        if (!file_exists($tempDir . ".changes/nextrelease/")) {
+            mkdir($tempDir . ".changes/nextrelease/", 0777, true);
         }
         $obj = new ChangelogBuilder($tempDir, '', false);
-        touch($tempDir . "/.changes/nextrelease/temp.json");
-        $this->assertEquals(1, count(preg_grep('/^([^.])/', scandir($tempDir . "/.changes/nextrelease/"))));
+        touch($tempDir . ".changes/nextrelease/temp.json");
+        $this->assertEquals(1, count(preg_grep('/^([^.])/', scandir($tempDir . ".changes/nextrelease/"))));
         $obj->cleanNextReleaseFolder();
-        $this->assertEquals(0, count(preg_grep('/^([^.])/', scandir($tempDir . "/.changes/nextrelease/"))));
+        $this->assertEquals(0, count(preg_grep('/^([^.])/', scandir($tempDir . ".changes/nextrelease/"))));
     }
 }
 
