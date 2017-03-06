46ff05300b2bc0ec7beb5d06a36477685e8164d8
Shashank Agarwal <agshasha@amazon.com>
2017-03-06 23:37:57 +0000

    Update models for release

--- src/data/cloudtrail/2013-11-01/paginators-1.json.php
+++ src/data/cloudtrail/2013-11-01/paginators-1.json.php
@@ -1,3 +1,3 @@
 <?php
 // This file was auto-generated from sdk-root/src/data/cloudtrail/2013-11-01/paginators-1.json
-return [ 'pagination' => [ 'DescribeTrails' => [ 'result_key' => 'trailList', ], 'LookupEvents' => [ 'input_token' => 'NextToken', 'output_token' => 'NextToken', 'limit_key' => 'MaxResults', 'result_key' => 'Events', ], ],];
+return [ 'pagination' => [ 'DescribeTrails' => [ 'result_key' => 'trailList', ], 'LookupEvents' => [ 'input_token' => 'NextToken', 'limit_key' => 'MaxResults', 'output_token' => 'NextToken', 'result_key' => 'Events', ], ],];
