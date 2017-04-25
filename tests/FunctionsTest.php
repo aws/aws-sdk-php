99083e9d9f4d4606b0ea96ff86ebf0f79dc36668
stickevi <stickevi@amazon.com>
2017-04-25 14:03:59 -0700

    Add PHPUnit coverage for functions.php. Clean up + mark coverage on existing tests and add several more for increased coverage.

--- tests/FunctionsTest.php
+++ tests/FunctionsTest.php
@@ -8,6 +8,9 @@ use Aws\S3\S3Client;
 
 class FunctionsTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @covers Aws\recursive_dir_iterator()
+     */
     public function testCreatesRecursiveDirIterator()
     {
         $iter = Aws\recursive_dir_iterator(__DIR__);
@@ -16,6 +19,9 @@ class FunctionsTest extends \PHPUnit_Framework_TestCase
         $this->assertContains(__FILE__, $files);
     }
 
+    /**
+     * @covers Aws\dir_iterator()
+     */
     public function testCreatesNonRecursiveDirIterator()
     {
         $iter = Aws\dir_iterator(__DIR__);
@@ -24,6 +30,9 @@ class FunctionsTest extends \PHPUnit_Framework_TestCase
         $this->assertContains('FunctionsTest.php', $files);
     }
 
+    /**
+     * @covers Aws\or_chain()
+     */
     public function testComposesOrFunctions()
     {
         $a = function ($a, $b) { return null; };
@@ -33,6 +42,9 @@ class FunctionsTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('+-', $comp('+', '-'));
     }
 
+    /**
+     * @covers Aws\or_chain()
+     */
     public function testReturnsNullWhenNonResolve()
     {
         $called = [];
@@ -44,6 +56,9 @@ class FunctionsTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(['a', 'b', 'c'], $called);
     }
 
+    /**
+     * @covers Aws\constantly()
+     */
     public function testCreatesConstantlyFunctions()
     {
         $fn = Aws\constantly('foo');
@@ -52,12 +67,17 @@ class FunctionsTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @expectedException \InvalidArgumentException
+     *
+     * @covers Aws\load_compiled_json()
      */
     public function testUsesJsonCompiler()
     {
         Aws\load_compiled_json('/path/to/not/here.json');
     }
 
+    /**
+     * @covers Aws\load_compiled_json()
+     */
     public function testUsesPhpCompilationOfJsonIfPossible()
     {
         $soughtData = ['foo' => 'bar'];
@@ -72,35 +92,109 @@ class FunctionsTest extends \PHPUnit_Framework_TestCase
         $this->assertSame($soughtData, Aws\load_compiled_json($jsonPath));
     }
 
-    public function filterTest()
+    /**
+     * @covers Aws\filter()
+     */
+    public function testFilter()
     {
         $data = [0, 1, 2, 3, 4];
-        $result = \Aws\filter($data, function ($v) { return $v % 2; });
+        $func = function ($v) { return $v % 2; };
+        $result = \Aws\filter($data, $func);
         $this->assertEquals([1, 3], iterator_to_array($result));
     }
 
-    public function mapTest()
+    /**
+     * @covers Aws\map()
+     */
+    public function testMap()
     {
         $data = [0, 1, 2, 3, 4];
         $result = \Aws\map($data, function ($v) { return $v + 1; });
         $this->assertEquals([1, 2, 3, 4, 5], iterator_to_array($result));
     }
 
-    public function flatmapTest()
+    /**
+     * @covers Aws\flatmap()
+     */
+    public function testFlatMap()
     {
-        $data = [[1, 2], [3], [], [4, 5]];
-        $xf = function ($value) { return array_sum($value); };
+        $data = ['Hello', 'World'];
+        $xf = function ($value) { return str_split($value); };
         $result = \Aws\flatmap($data, $xf);
-        $this->assertEquals([3, 3, 0, 9], iterator_to_array($result));
+        $this->assertEquals(['H', 'e', 'l', 'l', 'o', 'W', 'o', 'r', 'l', 'd'], iterator_to_array($result));
     }
 
-    public function partitionTest()
+    /**
+     * @covers Aws\partition()
+     */
+    public function testPartition()
     {
-        $data = [0, 1, 2, 3, 4, 5];
+        $data = [1, 2, 3, 4, 5];
         $result = \Aws\partition($data, 2);
         $this->assertEquals([[1, 2], [3, 4], [5]], iterator_to_array($result));
     }
 
+    /**
+     * @covers Aws\describe_type()
+     */
+    public function testDescribeObject()
+    {
+        $obj = new \stdClass();
+        $this->assertEquals('object(stdClass)', Aws\describe_type($obj));
+    }
+
+    /**
+     * @covers Aws\describe_type()
+     */
+    public function testDescribeArray()
+    {
+        $arr = [0, 1, 2];
+        $this->assertEquals('array(3)', Aws\describe_type($arr));
+    }
+
+    /**
+     * @covers Aws\describe_type()
+     */
+    public function testDescribeDoubleToFloat()
+    {
+        $double = (double)1.3;
+        $this->assertEquals('float(1.3)', Aws\describe_type($double));
+    }
+
+    /**
+     * @covers Aws\describe_type()
+     */
+    public function testDescribeType()
+    {
+        $this->assertEquals('int(1)', Aws\describe_type(1));
+        $this->assertEquals('string(4) "test"', Aws\describe_type("test"));
+    }
+
+    /**
+     * @covers Aws\default_http_handler()
+     */
+    public function testGuzzleV5HttpHandler()
+    {
+        if (!class_exists('GuzzleHttp\Ring\Core')) {
+            $this->markTestSkipped();
+        }
+        $this->assertInstanceOf(Aws\Handler\GuzzleV5\GuzzleHandler::class, Aws\default_http_handler());
+    }
+
+    /**
+     * @covers Aws\default_http_handler()
+     */
+    public function testGuzzleV6HttpHandler()
+    {
+        if (!class_exists('GuzzleHttp\Handler\StreamHandler')) {
+            $this->markTestSkipped();
+        }
+        $this->assertInstanceOf(Aws\Handler\GuzzleV6\GuzzleHandler::class, Aws\default_http_handler());
+    }
+
+    /**
+     * @covers Aws\serialize()
+     */
     public function testSerializesHttpRequests()
     {
         $mock = new MockHandler([new Result([])]);
@@ -129,5 +223,39 @@ class FunctionsTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($request->hasHeader('X-Amz-Content-Sha256'));
         $this->assertTrue($request->hasHeader('X-Amz-Date'));
         $this->assertEquals('123', (string) $request->getBody());
+    }
+
+    /**
+     * @covers Aws\manifest()
+     */
+    public function testServiceManifest()
+    {
+        $manifest = Aws\manifest('s3');
+        $data = [
+            'namespace' => 'S3',
+            'versions'  => [
+                'latest'     => '2006-03-01',
+                '2006-03-01' => '2006-03-01'
+            ],
+            'endpoint'  => 's3'
+        ];
+        $this->assertEquals($data, $manifest);
+    }
+
+    /**
+     * @covers Aws\manifest()
+     */
+    public function testAliasManifest()
+    {
+        $manifest = Aws\manifest('iotdataplane');
+        $data = [
+            'namespace' => 'IotDataPlane',
+            'versions'  => [
+                'latest'     => '2015-05-28',
+                '2015-05-28' => '2015-05-28'
+            ],
+            'endpoint'  => 'data.iot'
+        ];
+        $this->assertEquals($data, $manifest);
     }
 }
