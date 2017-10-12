4b73079721fd4c2f735b541ed2c53f008e10b9a1
shashank agarwal <agshasha@amazon.com>
2017-10-12 15:34:06 -0700

    Tag appended GCM Encrypting stream.

---
+++ src/Crypto/AesTaggedGcmEncryptingStream.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Aws\Crypto;
+
+use GuzzleHttp\Psr7;
+use Psr\Http\Message\StreamInterface;
+
+/**
+ * @internal Represents a stream of data to be gcm encrypted with a tag
+ * appended.
+ */
+class AesTaggedGcmEncryptingStream extends AesGcmEncryptingStream
+{
+    public function __construct(
+        StreamInterface $plaintext,
+        $key,
+        $initializationVector,
+        $aad = '',
+        $tagLength = 16,
+        $keySize = 256
+    ) {
+        parent::__construct(
+            $plaintext,
+            $key,
+            $initializationVector,
+            $aad,
+            $tagLength,
+            $keySize
+        );
+    }
+
+    public function createStream()
+    {
+        return Psr7\stream_for(
+            parent::createStream() . $this->getTag()
+        );
+    }
+
+}
