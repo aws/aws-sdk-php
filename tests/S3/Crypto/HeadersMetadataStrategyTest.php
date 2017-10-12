aa562319cf5b427d000edea2b1c5eabd141f2b6c
stickevi <stickevi@amazon.com>
2017-10-12 16:19:03 -0700

    Tests for both MetadataStrategyInterface implementers.

---
+++ tests/S3/Crypto/HeadersMetadataStrategyTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use Aws\S3\Crypto\HeadersMetadataStrategy;
+use Aws\Test\Crypto\UsesMetadataEnvelopeTrait;
+
+/**
+ * @covers Aws\S3\Crypto\HeadersMetadataStrategy
+ */
+class HeadersMetadataStrategyTest extends PHPUnit_Framework_TestCase
+{
+    use UsesMetadataEnvelopeTrait;
+
+    /**
+     * @dataProvider getMetadataFields
+     */
+    public function testSave($fields)
+    {
+        $strategy = new HeadersMetadataStrategy();
+        $args = ['Metadata' => []];
+
+        $updatedArgs = $strategy->save(
+            $this->getMetadataEnvelope($fields),
+            $args
+        );
+        foreach ($fields as $field => $value) {
+            $this->assertEquals($value, $updatedArgs['Metadata'][$field]);
+        }
+    }
+
+    /**
+     * @dataProvider getMetadataResult
+     */
+    public function testLoad($args, $metadata)
+    {
+        $strategy = new HeadersMetadataStrategy();
+        $envelope = $strategy->load($args);
+
+        foreach ($envelope as $field => $value) {
+            $this->assertEquals($value, $metadata[$field]);
+        }
+    }
+}
---
+++ tests/S3/Crypto/InstructionFileMetadataStrategyTest.php
@@ -0,0 +1,58 @@
+<?php
+
+use Aws\S3\Crypto\InstructionFileMetadataStrategy;
+use Aws\Result;
+use Aws\S3\S3Client;
+use Aws\Test\Crypto\UsesMetadataEnvelopeTrait;
+use Aws\Test\UsesServiceTrait;
+
+/**
+ * @covers Aws\S3\Crypto\InstructionFileMetadataStrategy
+ */
+class InstructionFileMetadataStrategyTest extends PHPUnit_Framework_TestCase
+{
+    use UsesMetadataEnvelopeTrait, UsesServiceTrait;
+
+    /**
+     * @dataProvider getMetadataFields
+     */
+    public function testSave($fields)
+    {
+        /** @var S3Client $client */
+        $client = $this->getTestClient('S3', []);
+        $strategy = new InstructionFileMetadataStrategy($client);
+        $args = [
+            'Bucket' => 'foo',
+            'Key' => 'bar',
+            'Metadata' => []
+        ];
+        $this->addMockResults($client, [
+            new Result(['ObjectURL' => 'file_url'])
+        ]);
+
+        $updatedArgs = $strategy->save(
+            $this->getMetadataEnvelope($fields),
+            $args
+        );
+        $this->assertNotEmpty($updatedArgs);
+        $this->assertEquals(0, count($updatedArgs['Metadata']));
+    }
+
+    /**
+     * @dataProvider getMetadataResult
+     */
+    public function testLoad($args, $metadata)
+    {
+        /** @var S3Client $client */
+        $client = $this->getTestClient('S3', []);
+        $strategy = new InstructionFileMetadataStrategy($client);
+        $this->addMockResults($client, [
+            new Result(['Body' => json_encode($metadata)])
+        ]);
+        $envelope = $strategy->load($args);
+
+        foreach ($envelope as $field => $value) {
+            $this->assertEquals($value, $metadata[$field]);
+        }
+    }
+}
