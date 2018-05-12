4442e43f2b33962f7d45d75b917d86f771bbb441
Howard Lopez <lopehowa@amazon.com>
2018-05-11 17:31:33 -0700

    Initial version of AbstractMonitoringMiddleware

---
+++ src/ClientSideMonitoring/AbstractMonitoringMiddleware.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace Aws\ClientSideMonitoring;
+
+use Aws\CommandInterface;
+use Aws\ResultInterface;
+use GuzzleHttp\Promise\Promise;
+use phpDocumentor\Reflection\Types\Resource_;
+use Psr\Http\Message\RequestInterface;
+
+/**
+ * Class AbstractMonitoringMiddleware
+ * @package Aws\ClientSideMonitoring
+ */
+abstract class AbstractMonitoringMiddleware
+{
+
+    /**
+     * UDP socket resource
+     * @var Resource
+     */
+    protected static $socket;
+
+
+    /**
+     * Socket settings. Port is mutable from options setting
+     *
+     * @var array
+     * @todo Change depending on resolution of CSM options with Bret
+     */
+    protected static $socket_settings = [
+        'address' => '127.0.0.1',
+        'domain' => AF_INET,
+        'port' => 31000,
+        'protocol' => SOL_UDP,
+        'type' => SOCK_DGRAM
+    ];
+
+
+    /**
+     * Next handler in middleware stack
+     * @var callable
+     */
+    protected $nextHandler;
+
+
+    /**
+     * Client-side monitoring options
+     * @var array
+     */
+    protected $options;
+
+
+    /**
+     * Constructor stores the passed in handler and options
+     *
+     * @param callable $handler
+     * @param array $options
+     * @todo Change depending on resolution of CSM options with Bret
+     */
+    public function __construct(callable $handler, array $options)
+    {
+        $this->nextHandler = $handler;
+        $this->options = $options;
+        if (!empty($options['port'])) {
+            self::$socket_settings['port'] = $options['port'];
+        }
+    }
+
+
+    /**
+     * Standard invoke pattern for middleware execution to be implemented by child classes
+     *
+     * @param  CommandInterface $cmd
+     * @param  RequestInterface $request
+     * @return Promise
+     */
+    public function __invoke(CommandInterface $cmd, RequestInterface $request) {
+
+        $handler = $this->nextHandler;
+
+        if (!empty($this->options['enabled'])) {
+            $eventData = $this->populateRequestEventData($cmd, $request);
+        } else {
+            $eventData = null;
+        }
+
+        return $handler($cmd, $request)->then(function(ResultInterface $result) use ($eventData) {
+            if (!empty($this->options['enabled'])) {
+                $eventData = $this->serializeEventData($this->populateResponseEventData($eventData, $result));
+                $this->sendEventData($eventData);
+            }
+            return $result;
+        });
+
+    }
+
+    /**
+     * Creates a UDP socket resource and stores it with the class, or retrieves it if already
+     * instantiated and connected. Handles error-checking and re-connecting if necessary.
+     * If $forceNewConnection is set to true, a new socket will be created.
+     *
+     * @param  bool $forceNewConnection
+     * @return Resource
+     */
+    private static function prepareSocket($forceNewConnection = false)
+    {
+        if (!is_resource(self::$socket) || $forceNewConnection || socket_last_error(self::$socket)) {
+            self::$socket = socket_create(
+                self::$socket_settings['domain'],
+                self::$socket_settings['type'],
+                self::$socket_settings['protocol']
+            );
+
+            socket_connect(self::$socket, self::$socket_settings['address'], self::$socket_settings['port']);
+            socket_clear_error(self::$socket);
+        }
+
+        return self::$socket;
+    }
+
+
+    /**
+     * Sends formatted monitoring event data via the UDP socket connection to the CSM agent endpoint
+     *
+     * @param string $eventData
+     * @return int
+     */
+    protected function sendEventData($eventData)
+    {
+        $socket = self::prepareSocket();
+        $result = socket_write($socket, $eventData, strlen($eventData));
+        if ($result === false) {
+            self::prepareSocket(true);
+        }
+        return $result;
+    }
+
+
+    /**
+     * Standard middleware wrapper function, with CSM options passed in
+     *
+     * @param  $options
+     * @return callable
+     */
+    public static function wrap($options)
+    {
+        return function (callable $handler) use ($options) {
+            $class = get_called_class();
+            return new $class($handler, $options);
+        };
+    }
+
+
+    /**
+     * Returns $eventData array with information from the request and command.
+     *
+     * @param CommandInterface $cmd
+     * @param RequestInterface $request
+     * @return array
+     */
+    abstract protected function populateRequestEventData(CommandInterface $cmd, RequestInterface $request);
+
+
+    /**
+     * Returns $eventData array with information from the response, including the calculation
+     * for attempt latency
+     *
+     * @param array $eventData
+     * @param ResultInterface $result
+     * @return array
+     */
+    abstract protected function populateResponseEventData(array $eventData, ResultInterface $result);
+
+
+    /**
+     * Serializes the event data with string length limitations, returning a JSON-formatted string.
+     *
+     * @param array $eventData
+     * @return string
+     */
+    abstract protected function serializeEventData(array $eventData);
+
+}
---
+++ src/ClientSideMonitoring/ApiCallAttemptMonitoringMiddleware.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Aws\ClientSideMonitoring;
+
+use Aws\CommandInterface;
+use Aws\ResultInterface;
+use Psr\Http\Message\RequestInterface;
+
+
+class ApiCallAttemptMonitoringMiddleware extends AbstractMonitoringMiddleware
+{
+
+    /**
+     * Returns $eventData array with information from the request and command.
+     *
+     * @param CommandInterface $cmd
+     * @param RequestInterface $request
+     * @return array
+     */
+    protected function populateRequestEventData(CommandInterface $cmd, RequestInterface $request)
+    {
+        return [];
+    }
+
+
+    /**
+     * Returns $eventData array with information from the response, including the calculation
+     * for attempt latency
+     *
+     * @param array $eventData
+     * @param ResultInterface $result
+     * @return array
+     */
+    protected function populateResponseEventData(array $eventData, ResultInterface $result)
+    {
+        return $eventData;
+    }
+
+
+    /**
+     * Serializes the event data with string length limitations, returning a JSON-formatted string.
+     *
+     * @param array $eventData
+     * @return string
+     */
+    protected function serializeEventData(array $eventData)
+    {
+        return '';
+    }
+
+}
