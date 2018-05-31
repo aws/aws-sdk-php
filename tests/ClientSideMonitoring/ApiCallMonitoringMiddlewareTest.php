513ee458205a2b02a955a23296be0b9a6cd9b9b4
Howard Lopez <lopehowa@amazon.com>
2018-05-31 16:15:02 -0700

    Update ApiCallMonitoringMiddlewareTest

--- tests/ClientSideMonitoring/ApiCallMonitoringMiddlewareTest.php
+++ tests/ClientSideMonitoring/ApiCallMonitoringMiddlewareTest.php
@@ -19,6 +19,18 @@ use PHPUnit\Framework\TestCase;
  */
 class ApiCallMonitoringMiddlewareTest extends TestCase
 {
+    protected function getConfiguration()
+    {
+        return new Configuration(true, 31000, 'TestApp');
+    }
+
+    protected function getCredentialProvider()
+    {
+        return CredentialProvider::fromCredentials(
+            new Credentials('testkey', 'testsecret', 'testtoken')
+        );
+    }
+
     /**
      * Used to get non-public methods for testing
      *
@@ -34,24 +46,29 @@ class ApiCallMonitoringMiddlewareTest extends TestCase
         return $method;
     }
 
+    protected function resetMiddlewareSocket()
+    {
+        $prepareSocket = $this->getMethod('prepareSocket');
+        $middleware = new ApiCallMonitoringMiddleware(function(){},
+            $this->getCredentialProvider(),
+            $this->getConfiguration(),
+            'test',
+            'test');
+        $prepareSocket->invokeArgs($middleware, array(true));
+    }
+
     public function testPopulatesMonitoringData()
     {
+        $this->resetMiddlewareSocket();
         $list = new HandlerList();
         $list->setHandler(function ($command, $request) use (&$called) {
             $called = true;
             return Promise\promise_for(new Result([]));
         });
 
-        $credentialProvider = CredentialProvider::fromCredentials(
-            new Credentials('testkey', 'testsecret', 'testtoken')
-        );
         $list->appendBuild(ApiCallMonitoringMiddleware::wrap(
-            $credentialProvider,
-            [
-                'enabled' => true,
-                'port' => 31000,
-                'client_id' => 'TestApp'
-            ],
+            $this->getCredentialProvider(),
+            $this->getConfiguration(),
             'us-east-1',
             'ec2'
         ));
@@ -65,21 +82,14 @@ class ApiCallMonitoringMiddlewareTest extends TestCase
             'InstanceCount' => 1,
         ]), new Request('POST', 'http://foo.com'))->wait();
         $this->assertTrue($called);
-        $this->assertEquals(
-            'RunScheduledInstances',
-            $response['@monitoringEvents'][0]['Api']
-        );
-        $this->assertEquals(
-            'ApiCall',
-            $response['@monitoringEvents'][0]['Type']
-        );
-        $this->assertEquals(
-            'ec2',
-            $response['@monitoringEvents'][0]['Service']
-        );
-        $this->assertEquals(
-            'TestApp',
-            $response['@monitoringEvents'][0]['ClientId']
+        $this->assertArraySubset(
+            [
+                'Api' => 'RunScheduledInstances',
+                'ClientId' => 'TestApp',
+                'Type' => 'ApiCall',
+                'Service' => 'ec2'
+            ],
+            $response['@monitoringEvents'][0]
         );
     }
 
