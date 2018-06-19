6d649a4fed5841df0924a014ab6a645e4a9e834c
Howard Lopez <lopehowa@amazon.com>
2018-06-19 12:41:28 -0700

    Add fallback for Service ID

--- tests/Api/ServiceTest.php
+++ tests/Api/ServiceTest.php
@@ -80,6 +80,39 @@ class ServiceTest extends TestCase
         $this->assertArrayHasKey('foo', $s->getOperations());
     }
 
+    public function serviceIdDataProvider()
+    {
+        return [
+            ['AWS New Service', '', '', 'New Service'],
+            ['AWS New Service', 'ANS', '', 'ANS'],
+            ['Amazon New Service', '', '', 'New Service'],
+            ['Amazon New Service', 'AWS NS', '', 'NS'],
+            ['Amazon New 555 Service', '', '', 'New 555 Service'],
+            ['555 Amazon New Service', '', '', 'New Service'],
+            [' 555 Amazon New Service', '', '', 'New Service'],
+            ['555 Amazon New Service', '555 NS', '', 'NS'],
+            ['  AWS @New-Service!', '', '', 'NewService'],
+            ['New Service Full Name', '  AWS @New-Service!', '', 'NewService'],
+            ['Fullname', 'Abbv', 'Supplied', 'Supplied'],
+        ];
+    }
+
+    /**
+     * @dataProvider serviceIdDataProvider
+     */
+    public function testGeneratesDefaultServiceIds($fullName, $abbv, $id, $expected)
+    {
+        $service = new Service([
+            'metadata' => [
+                'serviceFullName' => $fullName,
+                'serviceAbbreviation' => $abbv,
+                'serviceId' => $id
+            ]
+        ], function() {});
+        $serviceId = $service->getServiceId();
+        $this->assertEquals($expected, $serviceId);
+    }
+    
     /**
      * @expectedException \InvalidArgumentException
      */
