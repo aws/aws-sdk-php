c8c58780b8dd771477e70ac0b0df6bb8f79740c1
Howard Lopez <lopehowa@amazon.com>
2018-06-22 14:35:07 -0700

    Updated integration tests to new test cases format with mock service file

--- features/bootstrap/Aws/Test/Integ/ClientSideMonitoringContext.php
+++ features/bootstrap/Aws/Test/Integ/ClientSideMonitoringContext.php
@@ -2,6 +2,9 @@
 
 namespace Aws\Test\Integ;
 
+use Aws\Api\ApiProvider;
+use Aws\Api\Service;
+use Aws\AwsClient;
 use Aws\ClientSideMonitoring\ConfigurationProvider;
 use Aws\ClientSideMonitoring\Exception\ConfigurationException;
 use Aws\Command;
@@ -10,7 +13,6 @@ use Aws\Exception\AwsException;
 use Aws\MockHandler;
 use Aws\MonitoringEventsInterface;
 use Aws\Result;
-use Aws\ResultInterface;
 use Aws\Sdk;
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
@@ -38,10 +40,14 @@ class ClientSideMonitoringContext extends \PHPUnit_Framework_Assert
      */
     private $sdk;
 
-    private $testData;
     private $allExpectedEvents = [];
     private $allGeneratedEvents = [];
     private $defaultEnv = [];
+    private $sharedConfig;
+    private $testApiProvider;
+    private $testData;
+    private $testDir = __DIR__ . "/csm";
+    private $testServices = [];
 
     private $configKeys = [
         'region' => 'region',
@@ -90,12 +96,31 @@ class ClientSideMonitoringContext extends \PHPUnit_Framework_Assert
     }
 
     /**
-     * @Given I have loaded a test file called :filename
+     * @Given I have loaded a test manifest file called :filename
      */
-    public function iHaveLoadedATestFileCalled($filename)
+    public function iHaveLoadedATestManifestFileCalled($filename)
+    {
+        $manifest = json_decode(
+            file_get_contents("{$this->testDir}/data/{$filename}"),
+            true
+        );
+        $provider = ApiProvider::manifest("{$this->testDir}/data/", $manifest);
+        $serviceDirectories = glob("{$this->testDir}/data/*" , GLOB_ONLYDIR);
+        foreach ($serviceDirectories as $directory) {
+            $definition = $provider('api', basename($directory), 'latest');
+            $service = new Service($definition, $provider);
+            $this->testServices[$service->getServiceId()] = $service;
+        }
+        $this->testApiProvider = $provider;
+    }
+
+    /**
+     * @Given I have loaded a test cases file called :filename
+     */
+    public function iHaveLoadedATestCasesFileCalled($filename)
     {
         $this->testData = json_decode(
-            file_get_contents(__DIR__ . '/test_cases/' . $filename),
+            file_get_contents("{$this->testDir}/$filename"),
             true
         );
 
@@ -120,6 +145,7 @@ class ClientSideMonitoringContext extends \PHPUnit_Framework_Assert
                 'test-secret'
             );
         }
+        $this->sharedConfig = $sharedConfig;
         $this->sdk = new Sdk($sharedConfig);
     }
 
@@ -147,11 +173,34 @@ class ClientSideMonitoringContext extends \PHPUnit_Framework_Assert
                         putenv("{$key}={$value}");
                     }
                 }
+
+                $params = [];
+                if (!empty($case['configuration']['region'])) {
+                    $params['region'] = $case['configuration']['region'];
+                }
+                if (!empty($case['configuration']['accesskey'])) {
+                    $params['credentials'] = new Credentials(
+                        $case['configuration']['accesskey'],
+                        'test-secret'
+                    );
+                }
                 foreach ($case['apiCalls'] as $apiCall) {
-                    $client = $this->sdk->createClient($apiCall['serviceId']);
+                    /** @var Service $service */
+                    if (!empty($service = $this->testServices[$apiCall['serviceId']])) {
+                        $params['service'] = $service->getEndpointPrefix();
+                        $params['api_provider'] = $this->testApiProvider;
+                        $params += $this->sharedConfig;
+                        $client = new AwsClient($params);
+                    } else {
+                        $client = $this->sdk->createClient(
+                            $apiCall['serviceId'],
+                            $params
+                        );
+                    }
+
                     $list = $client->getHandlerList();
                     $command = new Command($apiCall['operationName'], $apiCall['params']);
-                    $request = new Request('POST', 'http://foo.com/bar/baz');
+                    $request = new Request('POST', (string) $client->getEndpoint());
                     $handler = new MockHandler();
                     foreach ($apiCall['attemptResponses'] as $attemptResponse) {
                         $response = $this->generateResponse($attemptResponse, $command);
@@ -166,9 +215,7 @@ class ClientSideMonitoringContext extends \PHPUnit_Framework_Assert
                     $handler = $list->resolve();
 
                     try {
-                        /**
-                         * @var Result $result
-                         */
+                        /** @var Result $result */
                         $result = $handler($command, $request)->wait();
                         $events = array_merge($events, $result->getMonitoringEvents());
                     } catch (\Exception $e) {
@@ -225,23 +272,20 @@ class ClientSideMonitoringContext extends \PHPUnit_Framework_Assert
             $actualEvent = $actual[$index];
             foreach ($expectedEvent as $key => $value) {
                 $this->assertArrayHasKey($key, $actualEvent);
-                switch ($value) {
-                    case "ANY_INT":
-                        $this->assertInternalType('int', $actualEvent[$key]);
-                        break;
-                    case "ANY_STR":
-                        $this->assertInternalType('string', $actualEvent[$key]);
-                        break;
-                    default:
-                        $this->assertEquals($value, $actualEvent[$key]);
-                }
+                $this->validateEventValue($value, $actualEvent[$key]);
             }
             $allowedFields = array_merge(
                 array_keys($expectedEvent),
-                $this->testData['defaults']['optionalEventFields'][$actualEvent['Type']]
+                array_keys($this->testData['defaults']['optionalEventFields'][$actualEvent['Type']])
             );
             foreach ($actualEvent as $key => $value) {
                 $this->assertTrue(in_array($key, $allowedFields));
+                if (in_array($key, $this->testData['defaults']['optionalEventFields'][$actualEvent['Type']])) {
+                    $this->validateEventValue(
+                        $this->testData['defaults']['optionalEventFields'][$actualEvent['Type']],
+                        $value
+                    );
+                }
             }
         }
     }
@@ -325,6 +369,20 @@ class ClientSideMonitoringContext extends \PHPUnit_Framework_Assert
                 exit();
             }
             $events[] = json_decode($buf, true);
+        }
+    }
+
+    private function validateEventValue($expected, $actual)
+    {
+        switch ($expected) {
+            case "ANY_INT":
+                $this->assertInternalType('int', $actual);
+                break;
+            case "ANY_STR":
+                $this->assertInternalType('string', $actual);
+                break;
+            default:
+                $this->assertEquals($expected, $actual);
         }
     }
 }
--- features/clientSideMonitoring/sendMonitoringEvents.feature
+++ features/clientSideMonitoring/sendMonitoringEvents.feature
@@ -4,7 +4,8 @@ Feature: Send client-side monitoring events
     Via UDP datagrams according to the specification when enabled
 
     Scenario: Verify generated and sent monitoring events
-        Given I have loaded a test file called "client_side_monitoring.json"
+        Given I have loaded a test manifest file called "manifest.json"
+        And I have loaded a test cases file called "cases.json"
         When I run the test cases with mocked responses against a test socket server
         Then the generated events should match the expected events
         And the received datagrams should match the expected events
