3a25d2b67cddd738bfbef8b9a64f7a959c3ebc59
Kevin Stich <stickevi@amazon.com>
2018-09-06 10:13:00 -0700

    Remove smoke.json file (#1618)

--- src/data/health/2016-08-04/smoke.json.php
+++
@@ -1,3 +0,0 @@
-<?php
-// This file was auto-generated from sdk-root/src/data/health/2016-08-04/smoke.json
-return [ 'version' => 1, 'defaultRegion' => 'us-east-1', 'testCases' => [ [ 'operationName' => 'DescribeEntityAggregates', 'input' => [], 'errorExpectedFromService' => false, ], ],];
