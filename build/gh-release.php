b5fb9b920d7cbe623fd43d6def9f03e505e3ce7d
Howard Lopez <lopehowa@amazon.com>
2018-10-18 09:47:05 -0700

    Use Guzzle retry middleware

--- build/gh-release.php
+++ build/gh-release.php
@@ -13,7 +13,6 @@ require __DIR__ . '/../vendor/autoload.php';
 use GuzzleHttp\Psr7\Uri;
 use GuzzleHttp\Psr7;
 
-$maxRetries = 3;
 $owner = 'aws';
 $repo = 'aws-sdk-php';
 $token = getenv('OAUTH_TOKEN') or die('An OAUTH_TOKEN environment variable is required');
@@ -24,78 +23,58 @@ $tag = $argv[1];
 chdir(dirname(__DIR__));
 $message = `chag contents -t "$tag"` or die('Chag could not find or parse the tag');
 
+// Add retry middleware
+$stack = \GuzzleHttp\HandlerStack::create();
+$stack->push(\GuzzleHttp\Middleware::retry(
+    function ($retries, $request, $response) {
+        $statusCode = $response->getStatusCode();
+        if ($retries < 3 && !in_array($statusCode, [200, 201, 202])) {
+            echo "Attempt failed with status code {$statusCode}: "
+                . $response->getBody();
+            return true;
+        }
+        return false;
+    },
+    function ($retries) {
+        return 1000 * (1 + $retries);
+    }
+));
+
 // Create a GitHub client.
 $client = new GuzzleHttp\Client([
     'base_uri' => 'https://api.github.com/',
     'headers' => ['Authorization' => "token $token"],
+    'handler' => $stack,
 ]);
 
 // Publish the release
-$url = retryWrapper(
-    'Publish the release',
-    function() use ($client, $tag, $message, $owner, $repo) {
-        $response = $client->post("repos/${owner}/${repo}/releases", [
-            'json' => [
-                'tag_name'   => $tag,
-                'name'       => "Version {$tag}",
-                'body'       => $message,
-            ]
-        ]);
+$response = $client->post("repos/${owner}/${repo}/releases", [
+    'json' => [
+        'tag_name'   => $tag,
+        'name'       => "Version {$tag}",
+        'body'       => $message,
+    ]
+]);
 
-        // Grab the location of the new release
-        $url = $response->getHeaderLine('Location');
-        echo "Release successfully published to: $url\n";
-        return $url;
-    },
-    $maxRetries
-);
+// Grab the location of the new release
+$url = $response->getHeaderLine('Location');
+echo "Release successfully published to: $url\n";
 
 // Uploads go to uploads.github.com
 $uploadUrl = new Uri($url);
 $uploadUrl = $uploadUrl->withHost('uploads.github.com');
 
 // Upload aws.zip
-retryWrapper(
-    'Upload aws.zip',
-    function() use ($client, $uploadUrl) {
-        $response = $client->post($uploadUrl . '/assets?name=aws.zip', [
-            'headers' => ['Content-Type' => 'application/zip'],
-            'body'    => Psr7\try_fopen(__DIR__ . '/artifacts/aws.zip', 'r')
-        ]);
-        echo "aws.zip uploaded to: " . json_decode($response->getBody(), true)['browser_download_url'] . "\n";
-        return $response;
-    },
-    $maxRetries
-);
+$response = $client->post($uploadUrl . '/assets?name=aws.zip', [
+    'headers' => ['Content-Type' => 'application/zip'],
+    'body'    => Psr7\try_fopen(__DIR__ . '/artifacts/aws.zip', 'r')
+]);
+echo "aws.zip uploaded to: " . json_decode($response->getBody(), true)['browser_download_url'] . "\n";
 
 // Upload aws.phar
-retryWrapper(
-    'Upload aws.phar',
-    function() use ($client, $uploadUrl) {
-        $response = $client->post($uploadUrl . '/assets?name=aws.phar', [
-            'headers' => ['Content-Type' => 'application/phar'],
-            'body'    => Psr7\try_fopen(__DIR__ . '/artifacts/aws.phar', 'r')
-        ]);
-        echo "aws.phar uploaded to: " . json_decode($response->getBody(), true)['browser_download_url'] . "\n";
-        return $response;
-    },
-    $maxRetries
-);
-
-function retryWrapper($label, callable $method, $maxRetries)
-{
-    $attempts = 0;
-    echo "{$label}...\n";
-    while ($attempts <= $maxRetries) {
-        $attempts++;
-        try {
-            return $method();
-        } catch (\Exception $e) {
-            if ($attempts > $maxRetries) {
-                throw $e;
-            }
-            echo "{$label} - Attempt #{$attempts} failed: " . $e->getMessage() . "\n";
-            usleep(1000000 * $attempts);
-        }
-    }
-}
+$response = $client->post($uploadUrl . '/assets?name=aws.phar', [
+    'headers' => ['Content-Type' => 'application/phar'],
+    'body'    => Psr7\try_fopen(__DIR__ . '/artifacts/aws.phar', 'r')
+]);
+echo "aws.phar uploaded to: " . json_decode($response->getBody(), true)['browser_download_url'] . "\n";
+return $response;
