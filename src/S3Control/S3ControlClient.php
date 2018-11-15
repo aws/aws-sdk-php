002bca8a30f6464d64e10bc636b237a4827125db
AwsSdkPhpAutomation <github-aws-sdk-php-automation@amazon.com>
2018-11-15 23:29:19 +0000

    Added S3ControlClient logic

---
+++ src/S3Control/S3ControlClient.php
@@ -0,0 +1,56 @@
+<?php
+namespace Aws\S3Control;
+
+use Aws\AwsClient;
+
+/**
+ * This client is used to interact with the **AWS S3 Control** service.
+ */
+class S3ControlClient extends AwsClient 
+{
+    public static function getArguments()
+    {
+        $args = parent::getArguments();
+        return $args + [
+            'use_dual_stack_endpoint' => [
+                'type' => 'config',
+                'valid' => ['bool'],
+                'doc' => 'Set to true to send requests to an S3 Control Dual Stack'
+                    . ' endpoint by default, which enables IPv6 Protocol.'
+                    . ' Can be enabled or disabled on individual operations by setting'
+                    . ' \'@use_dual_stack_endpoint\' to true or false.',
+                'default' => false,
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * In addition to the options available to
+     * {@see Aws\AwsClient::__construct}, S3ControlClient accepts the following
+     * option:
+     *
+     * - use_dual_stack_endpoint: (bool) Set to true to send requests to an S3
+     *   Control Dual Stack endpoint by default, which enables IPv6 Protocol.
+     *   Can be enabled or disabled on individual operations by setting
+     *   '@use_dual_stack_endpoint\' to true or false. Note:
+     *   you cannot use it together with an accelerate endpoint.
+     *
+     * @param array $args
+     */
+    public function __construct(array $args)
+    {
+        parent::__construct($args);
+        $stack = $this->getHandlerList();
+        $stack->appendBuild(
+            S3ControlEndpointMiddleware::wrap(
+                $this->getRegion(),
+                [
+                    'dual_stack' => $this->getConfig('use_dual_stack_endpoint'),
+                ]
+            ),
+            's3control.endpoint_middleware'
+        );
+    }
+}
