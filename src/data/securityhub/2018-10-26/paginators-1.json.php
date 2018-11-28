151290620da35ad26d8395943f9647b71f0a3fff
AwsSdkPhpAutomation <github-aws-sdk-php-automation@amazon.com>
2018-11-28 19:12:56 +0000

    Update models for release

---
+++ src/data/securityhub/2018-10-26/paginators-1.json.php
@@ -0,0 +1,3 @@
+<?php
+// This file was auto-generated from sdk-root/src/data/securityhub/2018-10-26/paginators-1.json
+return [ 'pagination' => [ 'GetFindings' => [ 'input_token' => 'NextToken', 'output_token' => 'NextToken', 'limit_key' => 'MaxResults', ], 'GetInsights' => [ 'input_token' => 'NextToken', 'output_token' => 'NextToken', 'limit_key' => 'MaxResults', ], 'ListEnabledProductsForImport' => [ 'input_token' => 'NextToken', 'output_token' => 'NextToken', 'limit_key' => 'MaxResults', ], ],];
