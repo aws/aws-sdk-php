c2f23d1487cbd20cfe57d3ba0bd4b7b3a24f06ff
Howard Lopez <lopehowa@amazon.com>
2018-12-17 14:20:07 -0800

    Configuration class

---
+++ src/EndpointDiscovery/Configuration.php
@@ -0,0 +1,36 @@
+<?php
+namespace Aws\EndpointDiscovery;
+
+class Configuration implements ConfigurationInterface
+{
+    private $cacheLimit;
+    private $enabled;
+
+    public function __construct($enabled, $cacheLimit)
+    {
+        $this->cacheLimit = filter_var($cacheLimit, FILTER_VALIDATE_INT);
+        if ($this->cacheLimit === false) {
+            throw new \InvalidArgumentException(
+                "'cache_limit' value must be an integer!");
+        }
+
+        // Unparsable $enabled flag errs on the side of disabling endpoint discovery
+        $this->enabled = filter_var($enabled, FILTER_VALIDATE_BOOLEAN);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCacheLimit()
+    {
+        return $this->cacheLimit;
+    }
+}
---
+++ src/EndpointDiscovery/ConfigurationInterface.php
@@ -0,0 +1,23 @@
+<?php
+namespace Aws\EndpointDiscovery;
+
+/**
+ * Provides access to endpoint discovery configuration options:
+ * 'enabled', 'cache_limit'
+ */
+interface ConfigurationInterface
+{
+    /**
+     * Checks whether or not endpoint discovery is enabled.
+     *
+     * @return bool
+     */
+    public function isEnabled();
+
+    /**
+     * Returns the cache limit, if available.
+     *
+     * @return string|null
+     */
+    public function getCacheLimit();
+}
