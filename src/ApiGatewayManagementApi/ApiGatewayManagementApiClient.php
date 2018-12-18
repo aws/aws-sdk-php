a31aeb36973e4eccf681af175461ad42067227f2
AwsSdkPhpAutomation <github-aws-sdk-php-automation@amazon.com>
2018-12-18 23:04:30 +0000

    Update models for release

---
+++ src/ApiGatewayManagementApi/ApiGatewayManagementApiClient.php
@@ -0,0 +1,11 @@
+<?php
+namespace Aws\ApiGatewayManagementApi;
+
+use Aws\AwsClient;
+
+/**
+ * This client is used to interact with the **AmazonApiGatewayManagementApi** service.
+ * @method \Aws\Result postToConnection(array $args = [])
+ * @method \GuzzleHttp\Promise\Promise postToConnectionAsync(array $args = [])
+ */
+class ApiGatewayManagementApiClient extends AwsClient {}
