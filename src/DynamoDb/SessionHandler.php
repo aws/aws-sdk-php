4cc4aa62a1dbceac978443013eeaaef84df1af33
Alessio Nobile <netziro@gmail.com>
2019-05-01 20:52:19 +0200

    NIT: spacing

--- src/DynamoDb/SessionHandler.php
+++ src/DynamoDb/SessionHandler.php
@@ -42,18 +42,18 @@ class SessionHandler implements \SessionHandlerInterface
      * Creates a new DynamoDB Session Handler.
      *
      * The configuration array accepts the following array keys and values:
-     * - table_name:               Name of table to store the sessions.
-     * - hash_key:                 Name of hash key in table. Default: "id".
-     * - data_attribute:           Name of the data attribute in table. Default: "data".
-     * - session_lifetime:         Lifetime of inactive sessions expiration.
-     * - session_lifetime_attribute: Name of the session life time attribute in table. Default: "expires".
-     * - consistent_read:          Whether or not to use consistent reads.
-     * - batch_config:             Batch options used for garbage collection.
-     * - locking:                  Whether or not to use session locking.
-     * - max_lock_wait_time:       Max time (s) to wait for lock acquisition.
-     * - min_lock_retry_microtime: Min time (µs) to wait between lock attempts.
-     * - max_lock_retry_microtime: Max time (µs) to wait between lock attempts.
-     * 
+     * - table_name:                    Name of table to store the sessions.
+     * - hash_key:                      Name of hash key in table. Default: "id".
+     * - data_attribute:                Name of the data attribute in table. Default: "data".
+     * - session_lifetime:              Lifetime of inactive sessions expiration.
+     * - session_lifetime_attribute:    Name of the session life time attribute in table. Default: "expires".
+     * - consistent_read:               Whether or not to use consistent reads.
+     * - batch_config:                  Batch options used for garbage collection.
+     * - locking:                       Whether or not to use session locking.
+     * - max_lock_wait_time:            Max time (s) to wait for lock acquisition.
+     * - min_lock_retry_microtime:      Min time (µs) to wait between lock attempts.
+     * - max_lock_retry_microtime:      Max time (µs) to wait between lock attempts.
+     *
      * You can find the full list of parameters and defaults within the trait
      * Aws\DynamoDb\SessionConnectionConfigTrait
      *
@@ -140,13 +140,13 @@ class SessionHandler implements \SessionHandlerInterface
         // PHP expects an empty string to be returned from this method if no
         // data is retrieved
         $this->dataRead = '';
-        
+
         // Get session data using the selected locking strategy
         $item = $this->connection->read($this->formatId($id));
-        
+
         $data_attribute = $this->connection->getDataAttribute();
         $session_lifetime_attribute = $this->connection->getSessionLifetimeAttribute();
-        
+
         // Return the data if it is not expired. If it is expired, remove it
         if (isset($item[$session_lifetime_attribute]) && isset($item[$data_attribute])) {
             $this->dataRead = $item[$data_attribute];
