2e4cc6406f4f92a41d1fbd660d277e730d5a254c
Howard Lopez <lopehowa@amazon.com>
2019-05-23 15:34:32 -0700

    Add aliases file

---
+++ src/data/aliases.json.php
@@ -0,0 +1,3 @@
+<?php
+// This file was auto-generated from sdk-root/src/data/aliases.json
+return [ 'cloudhsm' => [ '2014-05-30' => [ 'GetConfig' => 'GetConfigFiles', ], ], 'groundstation' => [ '2019-05-23' => [ 'GetConfig' => 'GetMissionProfileConfig', ], ],];
