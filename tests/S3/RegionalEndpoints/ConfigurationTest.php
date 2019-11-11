d308c6ff1d165c09597901fc2536527fb0947cc8
Howard Lopez <howardplopez@gmail.com>
2019-11-11 14:11:53 -0800

    Abstract configuration provider, S3 regional endpoint provider, configuration, tests

---
+++ tests/S3/RegionalEndpoints/ConfigurationTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Aws\Test\S3\RegionalEndpoints;
+
+use Aws\Sts\RegionalEndpoints\Configuration;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Aws\S3\RegionalEndpoint\Configuration
+ */
+class ConfigurationTest extends TestCase
+{
+    public function testGetsCorrectValues()
+    {
+        $config = new Configuration('regional');
+        $this->assertEquals('regional', $config->getEndpointsType());
+    }
+
+    public function testToArray()
+    {
+        $config = new Configuration('regional');
+        $expected = [
+            'endpoints_type' => 'regional',
+        ];
+        $this->assertEquals($expected, $config->toArray());
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Configuration parameter must either be 'legacy' or 'regional'.
+     */
+    public function testThrowsOnInvalidEndpointsType()
+    {
+        new Configuration('invalid_type');
+    }
+}
