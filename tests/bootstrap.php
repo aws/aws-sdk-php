603ea142ebbbbeaa4e6ee2dbca65dc13eb7282e1
Howard Lopez <howardplopez@gmail.com>
2019-12-04 12:54:07 -0800

    Add PHPUnit monkey patch on test bootstrap for PHP 7.4+

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -19,3 +19,11 @@ if (!class_exists('\PHPUnit\Framework\Error\Warning')) {
 
 // Clear out any previously compiled JMESPath files.
 JmesPath\Env::cleanCompileDir();
+
+// Patch PHPUnit class for PHP 7.4+ to avoid deprecation warning
+// Necessary because older versions of PHPUnit are no longer supported
+if (PHP_VERSION_ID >= 70400) {
+    $vendorGeneratorPath = __DIR__ . '/../vendor/phpunit/phpunit-mock-objects/src/Framework/MockObject/Generator.php';
+    $patchGeneratorPath = __DIR__ . '/bootstrap/PHPUnit_Framework_MockObject_Generator_7.4.php';
+    file_put_contents($vendorGeneratorPath, file_get_contents($patchGeneratorPath));
+}
