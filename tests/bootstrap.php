897c14e9a306b4a43dad921b8d7e50b47300cf72
Howard Lopez <38118252+howardlopez@users.noreply.github.com>
2019-12-06 09:42:48 -0800

    Phpunit 74 patch (#1918)
    * Add PHPUnit monkey patch on test bootstrap for PHP 7.4+
    
    * Add changelog
    
    * Add check for PHPUnit version for bootstrap patch
    
    * Remove 7.4snapshot from Travis, and make 7.4 not an allowed failure

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -19,3 +19,13 @@ if (!class_exists('\PHPUnit\Framework\Error\Warning')) {
 
 // Clear out any previously compiled JMESPath files.
 JmesPath\Env::cleanCompileDir();
+
+// Patch PHPUnit class for PHP 7.4+ and PHPUnit 5.x to avoid deprecation warning
+// Necessary because older versions of PHPUnit are no longer supported
+$version = PHPUnit_Runner_Version::id();
+$versionData = explode('.', $version);
+if (PHP_VERSION_ID >= 70400 && $versionData[0] == 5) {
+    $vendorGeneratorPath = __DIR__ . '/../vendor/phpunit/phpunit-mock-objects/src/Framework/MockObject/Generator.php';
+    $patchGeneratorPath = __DIR__ . '/bootstrap/PHPUnit_Framework_MockObject_Generator_7.4.php';
+    file_put_contents($vendorGeneratorPath, file_get_contents($patchGeneratorPath));
+}
