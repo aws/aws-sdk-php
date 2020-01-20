2059a0748736874590fe12456d6565683af871a2
Howard Lopez <howardplopez@gmail.com>
2020-01-20 14:28:58 -0800

    Add configuration validation

--- src/RetryConfiguration/Configuration.php
+++ src/RetryConfiguration/Configuration.php
@@ -1,13 +1,30 @@
 <?php
 namespace Aws\RetryConfiguration;
 
+use Aws\RetryConfiguration\Exception\ConfigurationException;
+
 class Configuration implements ConfigurationInterface
 {
     private $mode;
     private $maxAttempts;
+    private $validModes = [
+        'legacy',
+        'standard',
+        'adaptive'
+    ];
 
     public function __construct($mode = 'legacy', $maxAttempts = 3)
     {
+        $mode = strtolower($mode);
+        if (!in_array($mode, $this->validModes)) {
+            throw new ConfigurationException("'{$mode}' is not a valid mode."
+                . " The mode has to be 'legacy', 'standard', or 'adaptive'.");
+        }
+        if (!is_int($maxAttempts) || $maxAttempts < 1) {
+            throw new ConfigurationException("The 'maxAttempts' parameter has"
+                . " to be an integer >= 1.");
+        }
+
         $this->mode = $mode;
         $this->maxAttempts = $maxAttempts;
     }
