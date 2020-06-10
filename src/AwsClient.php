d3694e43e172e623211940cdbb2922ba24ee87c7
= <=>
2020-06-10 17:10:39 -0400

    tRevert "made AwsClient's getsignature[rovider method public"
    This reverts commit 9a62092af615e2e3c362dc51fd902d6f58ba35ed.
    
    Fixing git history

--- src/AwsClient.php
+++ src/AwsClient.php
@@ -277,7 +277,7 @@ class AwsClient implements AwsClientInterface
      *
      * @return callable
      */
-    final public function getSignatureProvider()
+    final protected function getSignatureProvider()
     {
         return $this->signatureProvider;
     }
