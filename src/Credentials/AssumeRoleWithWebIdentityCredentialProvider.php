2230b59fd629fd9d43111b797cdf4192ed470582
Sam Remis <sjremis94@gmail.com>
2020-09-22 00:03:15 -0400

    reworked commit

--- src/Credentials/AssumeRoleWithWebIdentityCredentialProvider.php
+++ src/Credentials/AssumeRoleWithWebIdentityCredentialProvider.php
@@ -94,12 +94,16 @@ class AssumeRoleWithWebIdentityCredentialProvider
             $result = null;
             while ($result == null) {
                 try {
-                    $token = file_get_contents($this->tokenFile);
+                    $token = is_readable($this->tokenFile)
+                        ? file_get_contents($this->tokenFile)
+                        : false;
                     if (false === $token) {
                         clearstatcache(true, dirname($this->tokenFile) . "/" . readlink($this->tokenFile));
                         clearstatcache(true, dirname($this->tokenFile) . "/" . dirname(readlink($this->tokenFile)));
                         clearstatcache(true, $this->tokenFile);
-                        $token = file_get_contents($this->tokenFile);   
+                        $token = is_readable($this->tokenFile)
+                            ? file_get_contents($this->tokenFile)
+                            : false;
                     }
                 } catch (\Exception $exception) {
                     throw new CredentialsException(
