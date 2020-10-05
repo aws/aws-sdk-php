4ec75c406f3da772d3929129147393116f989072
Sam Remis <sjremis94@gmail.com>
2020-10-05 13:36:28 -0400

    Revert "suppress file warning (#2103)"
    This reverts commit b657810558194c337664922e38979503dbc89bec.

--- src/Credentials/AssumeRoleWithWebIdentityCredentialProvider.php
+++ src/Credentials/AssumeRoleWithWebIdentityCredentialProvider.php
@@ -94,19 +94,12 @@ class AssumeRoleWithWebIdentityCredentialProvider
             $result = null;
             while ($result == null) {
                 try {
-                    $token = is_readable($this->tokenFile)
-                        ? file_get_contents($this->tokenFile)
-                        : false;
+                    $token = file_get_contents($this->tokenFile);
                     if (false === $token) {
                         clearstatcache(true, dirname($this->tokenFile) . "/" . readlink($this->tokenFile));
                         clearstatcache(true, dirname($this->tokenFile) . "/" . dirname(readlink($this->tokenFile)));
                         clearstatcache(true, $this->tokenFile);
-                        if (!is_readable($this->tokenFile)) {
-                            throw new CredentialsException(
-                                "Unreadable tokenfile at location {$this->tokenFile}"
-                            );
-                        }
-                        $token = file_get_contents($this->tokenFile);
+                        $token = file_get_contents($this->tokenFile);   
                     }
                 } catch (\Exception $exception) {
                     throw new CredentialsException(
