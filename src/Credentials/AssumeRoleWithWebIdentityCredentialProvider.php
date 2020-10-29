f3fc284987a891907a4f166d2585fbd89171ba58
Sam Remis <sjremis94@gmail.com>
2020-10-29 17:56:49 -0400

    Update AssumeRoleWithWebIdentityCredentialProvider.php

--- src/Credentials/AssumeRoleWithWebIdentityCredentialProvider.php
+++ src/Credentials/AssumeRoleWithWebIdentityCredentialProvider.php
@@ -94,13 +94,17 @@ class AssumeRoleWithWebIdentityCredentialProvider
             $result = null;
             while ($result == null) {
                 try {
-                    $token = file_get_contents($this->tokenFile);
-                    if (false === $token) {
+                    $token = is_readable($this->tokenFile)
+                        ? file_get_contents($this->tokenFile)
+                        : false;                    if (false === $token) {
                         clearstatcache(true, dirname($this->tokenFile) . "/" . readlink($this->tokenFile));
                         clearstatcache(true, dirname($this->tokenFile) . "/" . dirname(readlink($this->tokenFile)));
                         clearstatcache(true, $this->tokenFile);
-                        $token = file_get_contents($this->tokenFile);   
-                    }
+                        if (!is_readable($this->tokenFile)) {
+                            throw new CredentialsException(
+                                "Unreadable tokenfile at location {$this->tokenFile}"
+                            );
+                        }                    }
                 } catch (\Exception $exception) {
                     throw new CredentialsException(
                         "Error reading WebIdentityTokenFile from " . $this->tokenFile,
