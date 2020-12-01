2d43ff4653b856d576a0040906ca44c5597f06ca
Sam Remis <sjremis94@gmail.com>
2020-12-01 17:27:33 -0500

    remove profile

--- src/Profile/Exception/ProfileException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Aws\Profile\Exception;
-
-use Aws\Exception\AwsException;
-
-/**
- * Represents an error interacting with the **Amazon Connect Customer Profiles** service.
- */
-class ProfileException extends AwsException {}
--- src/data/profile/2020-08-15/examples-1.json.php
+++
@@ -1,3 +0,0 @@
-<?php
-// This file was auto-generated from sdk-root/src/data/profile/2020-08-15/examples-1.json
-return [ 'version' => '1.0', 'examples' => [],];
