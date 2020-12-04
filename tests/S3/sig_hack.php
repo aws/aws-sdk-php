11b2fd5172e3f26eade6bcf18c16a1718457e88c
Sam Remis <sjremis94@gmail.com>
2020-12-04 14:26:08 -0500

    changed require to require once

--- tests/S3/sig_hack.php
+++ tests/S3/sig_hack.php
@@ -1,7 +1,7 @@
 <?php
 namespace Aws\S3;
 
-require __DIR__ . '/../Signature/sig_hack.php';
+require_once __DIR__ . '/../Signature/sig_hack.php';
 
 // Hack gmdate() to returned the canned result.
 function gmdate($format, $ts = null)
