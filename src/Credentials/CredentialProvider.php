49a3da26315e163676c86ffe7fc7cd74a537dd8a
Stephen Cuppett <scuppett@redhat.com>
2020-12-14 19:46:08 -0500

    Correcting documentation stanza of the defaultProvider

--- src/Credentials/CredentialProvider.php
+++ src/Credentials/CredentialProvider.php
@@ -54,14 +54,15 @@ class CredentialProvider
     const ENV_SHARED_CREDENTIALS_FILE = 'AWS_SHARED_CREDENTIALS_FILE';
 
     /**
-     * Create a default credential provider that first checks for environment
-     * variables, then checks for the "default" profile in ~/.aws/credentials,
+     * Create a default credential provider that first checks for
+     * environment variables,
+     * then checks for the "default" profile in ~/.aws/credentials,
+     * then check for credential_process in the "default" profile in ~/.aws/credentials,
      * then checks for "profile default" profile in ~/.aws/config (which is
-     * the default profile of AWS CLI), then tries to make a GET Request to
-     * fetch credentials if Ecs environment variable is presented, then checks
-     * for credential_process in the "default" profile in ~/.aws/credentials,
-     * then for credential_process in the "default profile" profile in
-     * ~/.aws/config, and finally checks for EC2 instance profile credentials.
+     * the default profile of AWS CLI),
+     * then for credential_process in the "default profile" profile in ~/.aws/config,
+     * then tries to make a GET Request to fetch credentials if ECS environment variable is presented,
+     * finally checks for EC2 instance profile credentials.
      *
      * This provider is automatically wrapped in a memoize function that caches
      * previously provided credentials.
@@ -96,11 +97,11 @@ class CredentialProvider
                 $config
             );
             $defaultChain['ini'] = self::ini();
+            $defaultChain['process_credentials'] = self::process();
             $defaultChain['ini_config'] = self::ini(
                 'profile default',
                 self::getHomeDir() . '/.aws/config'
             );
-            $defaultChain['process_credentials'] = self::process();
             $defaultChain['process_config'] = self::process(
                 'profile default',
                 self::getHomeDir() . '/.aws/config'
