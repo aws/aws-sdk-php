994d02df1cbb4f08e90708a3e24f15a64e6f8a15
SamRemis <sjremis94@gmail.com>
2020-12-16 15:47:46 -0500

    Guzzle handler now accepts errors (#2163)
    * Guzzle handler now accepts errors
    
    * add changelog
    
    * Update HandlerTest.php
    
    * Update HandlerTest.php
    
    * Update GuzzleHandler.php
    
    * Update GuzzleHandler.php

--- tests/Handler/GuzzleV6/HandlerTest.php
+++ tests/Handler/GuzzleV6/HandlerTest.php
@@ -109,4 +109,37 @@ class HandlerTest extends TestCase
 
         $this->assertTrue($wasCalled);
     }
+
+    public function testHandlerWorksWithErroredRequest()
+    {
+        $wasRejected = false;
+        $request = new Request('PUT', 'http://example.com');
+        $mock = new MockHandler(
+            [
+                new RejectionException(
+                    new \Error('error message')
+                )
+            ]
+        );
+        $client = new Client(['handler' => $mock]);
+        $handler = new GuzzleHandler($client);
+
+        $promise = $handler(new Request('PUT', 'http://example.com'));
+        $promise->then(null, function (array $error) use (&$wasRejected) {
+            $wasRejected = true;
+        });
+
+        try {
+            $promise->wait();
+            $this->fail('An exception should have been thrown.');
+        } catch (RejectionException $e) {
+            $error = $e->getReason();
+            $this->assertInstanceOf(\Error::class, $error['exception']->getReason());
+            $this->assertFalse($error['connection_error']);
+            $this->assertContains("error message", $error['exception']->getMessage());
+        }
+
+        $this->assertTrue($wasRejected, 'Reject callback was not triggered.');
+    }
+
 }
