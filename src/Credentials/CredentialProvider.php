f35ffa99419d9d765d9c237fdf94a560fdf27f3a
Sam Remis <sjremis94@gmail.com>
2020-12-16 18:03:20 -0500

    Update CredentialProvider.php

--- src/Credentials/CredentialProvider.php
+++ src/Credentials/CredentialProvider.php
@@ -102,6 +102,9 @@ class CredentialProvider
             );
         }
 
+        $shouldUseEcsCredentialsProvider = getenv(EcsCredentialProvider::ENV_URI);
+        // getenv() is not thread safe - fall back to $_SERVER
+        if ($shouldUseEcsCredentialsProvider === false) {
             $shouldUseEcsCredentialsProvider = isset($_SERVER[EcsCredentialProvider::ENV_URI])
                 ? $_SERVER[EcsCredentialProvider::ENV_URI]
                 : false;
