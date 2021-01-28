7720d26c49e4baf6325faf26e41436600659fc33
Sam Remis <sjremis94@gmail.com>
2021-01-28 17:12:47 -0500

    bugfix to update content type in lexmodelsv2

--- src/LexModelsV2/LexModelsV2Client.php
+++ src/LexModelsV2/LexModelsV2Client.php
@@ -2,6 +2,8 @@
 namespace Aws\LexModelsV2;
 
 use Aws\AwsClient;
+use Aws\CommandInterface;
+use Psr\Http\Message\RequestInterface;
 
 /**
  * This client is used to interact with the **Amazon Lex Model Building V2** service.
@@ -86,4 +88,37 @@ use Aws\AwsClient;
  * @method \Aws\Result updateSlotType(array $args = [])
  * @method \GuzzleHttp\Promise\Promise updateSlotTypeAsync(array $args = [])
  */
-class LexModelsV2Client extends AwsClient {}
+class LexModelsV2Client extends AwsClient {
+    public function __construct(array $args)
+    {
+        parent::__construct($args);
+
+        // Setup middleware.
+        $stack = $this->getHandlerList();
+        $stack->appendBuild($this->updateContentType(), 'models.lex.v2.updateContentType');
+    }
+
+    /**
+     * Creates a middleware that updates the Content-Type header for all requests with bodies.
+     *
+     * @return callable
+     */
+    private function updateContentType()
+    {
+        return function (callable $handler) {
+            return function (
+                CommandInterface $command,
+                RequestInterface $request = null
+            ) use ($handler) {
+                $contentType = $request->getHeader('Content-Type');
+                if (!empty($contentType) && $contentType[0] == 'application/json') {
+                    return $handler($command, $request->withHeader(
+                        'Content-Type',
+                        'application/x-amz-json-1.1'
+                    ));
+                }
+                return $handler($command, $request);
+            };
+        };
+    }
+}
