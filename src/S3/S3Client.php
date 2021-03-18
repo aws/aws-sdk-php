335ed5a956f096bc56842ffe8b0461ef96ff3739
SamRemis <sjremis94@gmail.com>
2021-03-18 16:38:56 -0400

    Feature s3 object lambda (#2206)
    * feature-s3-object-lambda

--- src/S3/S3Client.php
+++ src/S3/S3Client.php
@@ -365,7 +365,10 @@ class S3Client extends AwsClient implements S3ClientInterface
                     [
                         'dual_stack' => $this->getConfig('use_dual_stack_endpoint'),
                         'accelerate' => $this->getConfig('use_accelerate_endpoint'),
-                        'path_style' => $this->getConfig('use_path_style_endpoint')
+                        'path_style' => $this->getConfig('use_path_style_endpoint'),
+                        'endpoint' => isset($args['endpoint'])
+                            ? $args['endpoint']
+                            : null
                     ]
                 ),
                 's3.endpoint_middleware'
