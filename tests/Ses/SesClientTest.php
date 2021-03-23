05aaea1780fdafb18a49f840dd59735cff66e397
Sam Remis <sjremis94@gmail.com>
2021-03-22 23:06:22 -0400

    bugfix-generate-smtp-v4

--- tests/Ses/SesClientTest.php
+++ tests/Ses/SesClientTest.php
@@ -21,4 +21,19 @@ class SesClientTest extends TestCase
             SesClient::generateSmtpPassword($testCreds)
         );
     }
+
+    public function testCanGenerateSmtpPasswordV4FromCredentials()
+    {
+        $testCreds = new Credentials(
+            'AKIAIOSFODNN7EXAMPLE',
+            'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+        );
+        // Created using sample code on: http://docs.aws.amazon.com/ses/latest/DeveloperGuide/smtp-credentials.html#smtp-credentials-convert
+        $expectedPassword = 'BMPPgbAHoFVbze1Ud0oA5uNdXRwUXUZ+qOwphGJbl/cS';
+
+        $this->assertSame(
+            $expectedPassword,
+            SesClient::generateSmtpPasswordV4($testCreds, 'us-east-2')
+        );
+    }
 }
