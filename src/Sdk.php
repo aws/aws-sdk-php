05b0b53073f9cddc3554a297fd5f18e37bcd4876
AwsSdkPhpAutomation <github-aws-sdk-php-automation@amazon.com>
2021-04-28 18:14:15 +0000

    Update models for release

--- src/Sdk.php
+++ src/Sdk.php
@@ -364,6 +364,8 @@ namespace Aws;
  * @method \Aws\MultiRegionClient createMultiRegionNetworkFirewall(array $args = [])
  * @method \Aws\NetworkManager\NetworkManagerClient createNetworkManager(array $args = [])
  * @method \Aws\MultiRegionClient createMultiRegionNetworkManager(array $args = [])
+ * @method \Aws\NimbleStudio\NimbleStudioClient createNimbleStudio(array $args = [])
+ * @method \Aws\MultiRegionClient createMultiRegionNimbleStudio(array $args = [])
  * @method \Aws\OpsWorks\OpsWorksClient createOpsWorks(array $args = [])
  * @method \Aws\MultiRegionClient createMultiRegionOpsWorks(array $args = [])
  * @method \Aws\OpsWorksCM\OpsWorksCMClient createOpsWorksCM(array $args = [])
