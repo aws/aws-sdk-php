867fdacc607aec5beface26300732720a2c9b3a8
Sam Remis <sjremis94@gmail.com>
2021-09-03 15:44:08 -0400

    fix client casing

--- build/ServiceBuilder.php
+++ build/ServiceBuilder.php
@@ -33,7 +33,9 @@ class ServiceBuilder
     {
         $property = lcfirst($fileType) . 'Path';
         $method = 'generateDefault' . $fileType;
-        if (!file_exists($this->{$property})) {
+        if (!file_exists($this->{$property})
+            && !file_exists(lcfirst($this->{$property}))
+        ) {
             if (!is_dir(dirname($this->{$property}))) {
                 mkdir(dirname($this->{$property}), 0755, true);
             }
