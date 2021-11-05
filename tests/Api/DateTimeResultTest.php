57ffe0268b2b9b03bf0a033227ed98c2d8c08b92
Sam Remis <sjremis94@gmail.com>
2021-11-05 11:41:52 -0400

    Fixed timestamp issue

--- tests/Api/DateTimeResultTest.php
+++ tests/Api/DateTimeResultTest.php
@@ -24,6 +24,17 @@ class DateTimeResultTest extends TestCase
         $this->assertSame(\PHP_VERSION_ID < 56000 ? '16344171.000000' : '16344171.123432', $d->format('U.u'));
     }
 
+    public function testCreatesFromEpochFloatInForeignDecimalSeparatorFormat()
+    {
+        $oldLocale = setlocale(LC_ALL, 0);
+        setlocale(LC_ALL, 'es_ES.UTF-8');
+        $t = 16344171.123432;
+        $d = DateTimeResult::fromEpoch($t);
+        $this->assertSame('16344171', $d->format('U'));
+        $this->assertSame(\PHP_VERSION_ID < 56000 ? '16344171.000000' : '16344171.123432', $d->format('U.u'));
+        setlocale(LC_ALL, $oldLocale);
+    }
+
     public function testCastToIso8601String()
     {
         $t = time();
