9c7801eba9319e939b89f407528439d04045d98d
SamRemis <sjremis94@gmail.com>
2022-02-28 13:51:04 -0500

    Bugfix missing content md5 (#2390)
    Fix issue with content-md5 header not being added

--- src/S3/ApplyChecksumMiddleware.php
+++ src/S3/ApplyChecksumMiddleware.php
@@ -91,24 +91,20 @@ class ApplyChecksumMiddleware
 
         if (!empty($checksumInfo)) {
         //if the checksum member is absent, check if it's required
-        $checksumRequired = isset($op['requestChecksumRequired'])
-            ? $op['requestChecksumRequired']
+        $checksumRequired = isset($checksumInfo['requestChecksumRequired'])
+            ? $checksumInfo['requestChecksumRequired']
             : null;
-            if (!empty($checksumRequired)) {
-                throw new InvalidArgumentException(
-                    "Command {$name} is missing required checksum input variable:"
-                    . " {$checksumMemberName}"
+            if (!empty($checksumRequired) && !$request->hasHeader('Content-MD5')) {
+                // Set the content MD5 header for operations that require it.
+                $request = $request->withHeader(
+                    'Content-MD5',
+                    base64_encode(Psr7\Utils::hash($body, 'md5', true))
                 );
-          }
+                return $next($command, $request);
+            }
         }
 
-        if (!empty($op['httpChecksumRequired']) && !$request->hasHeader('Content-MD5')) {
-            // Set the content MD5 header for operations that require it.
-            $request = $request->withHeader(
-                'Content-MD5',
-                base64_encode(Psr7\Utils::hash($body, 'md5', true))
-            );
-        } elseif (in_array($name, self::$sha256) && $command['ContentSHA256']) {
+        if (in_array($name, self::$sha256) && $command['ContentSHA256']) {
             // Set the content hash header if provided in the parameters.
             $request = $request->withHeader(
                 'X-Amz-Content-Sha256',
