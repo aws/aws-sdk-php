9cf3900b8159e3964148831aef50be32fc90aeb4
Sean O'Brien <obrien.sean.dev@gmail.com>
2022-05-26 21:10:15 -0500

    bugfix: remove EMRServerlessWebService

--- src/EMRServerlessWebService/Exception/EMRServerlessWebServiceException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Aws\EMRServerlessWebService\Exception;
-
-use Aws\Exception\AwsException;
-
-/**
- * Represents an error interacting with the **EMR Serverless Web Service** service.
- */
-class EMRServerlessWebServiceException extends AwsException {}
