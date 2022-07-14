65a9a8883593598a5506a33ab2ca60373a8bf18f
AwsSdkPhpAutomation <github-aws-sdk-php-automation@amazon.com>
2022-07-14 13:16:38 -0400

    streamwrapper catch up

--- src/S3/StreamWrapper.php
+++ src/S3/StreamWrapper.php
@@ -109,9 +109,9 @@ class StreamWrapper
      */
     public static function register(
         S3ClientInterface $client,
-        $protocol = 's3',
+                          $protocol = 's3',
         CacheInterface $cache = null,
-        $v2Existence = false
+                          $v2Existence = false
     ) {
         self::$useV2Existence = $v2Existence;
         if (in_array($protocol, stream_get_wrappers())) {
@@ -135,7 +135,10 @@ class StreamWrapper
 
     public function stream_close()
     {
-        if ($this->body->getSize() === 0 && !($this->isFlushed)) {
+        if (!$this->isFlushed
+            && empty($this->body->getSize())
+            && $this->mode !== 'r'
+        ) {
             $this->stream_flush();
         }
         $this->body = $this->cache = null;
@@ -168,6 +171,14 @@ class StreamWrapper
 
     public function stream_flush()
     {
+        // Check if stream body size has been
+        // calculated via a flush or close
+        if($this->body->getSize() === null && $this->mode !== 'r') {
+            return $this->triggerError(
+                "Unable to determine stream size. Did you forget to close or flush the stream?"
+            );
+        }
+
         $this->isFlushed = true;
         if ($this->mode == 'r') {
             return false;
@@ -558,9 +569,9 @@ class StreamWrapper
             );
             // Delete the original object
             $this->getClient()->deleteObject([
-                'Bucket' => $partsFrom['Bucket'],
-                'Key'    => $partsFrom['Key']
-            ] + $options);
+                    'Bucket' => $partsFrom['Bucket'],
+                    'Key'    => $partsFrom['Key']
+                ] + $options);
             return true;
         });
     }
