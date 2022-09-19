cede66df4abe4056a767c62944c01dbf55b1c4b9
Sean O'Brien <obrien.sean.dev@gmail.com>
2022-09-19 17:43:55 -0400

    fix isvirtualhostables3bucket

--- src/EndpointV2/Ruleset/RulesetStandardLibrary.php
+++ src/EndpointV2/Ruleset/RulesetStandardLibrary.php
@@ -266,7 +266,7 @@ class RulesetStandardLibrary
     public function isVirtualHostableS3Bucket($bucketName, $allowSubdomains)
     {
         if ((is_null($bucketName)
-            || strlen($bucketName) < 3 || strlen($bucketName > 63))
+            || (strlen($bucketName) < 3 || strlen($bucketName) > 63))
             || preg_match(self::IPV4_RE, $bucketName)
             || strtolower($bucketName) !== $bucketName
         ) {
