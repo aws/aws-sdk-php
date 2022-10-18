8d3886b64f51aa1a89d2a737ea82f285bd7bf5d8
SamRemis <sjremis94@amazon.com>
2022-10-18 17:56:38 -0400

    Update compile-json.php

--- build/compile-json.php
+++ build/compile-json.php
@@ -7,7 +7,14 @@ require __DIR__ . '/../vendor/autoload.php';
  */
 $dataFilesIterator = \Aws\recursive_dir_iterator(realpath(__DIR__ . '/../src/data'));
 
+
 foreach (new RegexIterator($dataFilesIterator, '/\.json$/') as $dataFile) {
     (new JsonCompiler($dataFile))
         ->compile("$dataFile.php");
+}
+foreach (new RegexIterator($dataFilesIterator, '/endpoint-.*\.json(\.php)?$/') as $dataFile) {
+    $gzdata = gzencode(file_get_contents($dataFile));
+    file_put_contents( $dataFile.".gz", $gzdata);
+    unlink($dataFile);
+    echo $dataFile . "\n";
 }
