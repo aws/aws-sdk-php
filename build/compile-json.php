39a52e33fa903efe5d70284cc74425e8c3248aac
SamRemis <sjremis94@gmail.com>
2022-10-19 10:14:56 -0400

    Feature zip endpoints v2 files (#2542)
    * Update compile-json.php

--- build/compile-json.php
+++ build/compile-json.php
@@ -6,11 +6,11 @@ require __DIR__ . '/../vendor/autoload.php';
  * arrays, and writes those files to $originalFileName.json.php.
  */
 $dataFilesIterator = \Aws\recursive_dir_iterator(realpath(__DIR__ . '/../src/data'));
-
 foreach (new RegexIterator($dataFilesIterator, '/\.json$/') as $dataFile) {
     (new JsonCompiler($dataFile))
         ->compile("$dataFile.php");
 }
+$dataFilesIterator = \Aws\recursive_dir_iterator(realpath(__DIR__ . '/../src/data'));
 foreach (new RegexIterator($dataFilesIterator, '/endpoint-.*\.json(\.php)?$/') as $dataFile) {
     $gzdata = gzencode(file_get_contents($dataFile));
     file_put_contents( $dataFile.".gz", $gzdata);
