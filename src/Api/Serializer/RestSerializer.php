b246cfd4a1ba91dbfc0d411c581edf5dba1be401
Sean O'Brien <60306702+stobrien89@users.noreply.github.com>
2022-11-15 16:56:45 -0500

    bugfix: account for leading / in rest serializer (#2564)

--- src/Api/Serializer/RestSerializer.php
+++ src/Api/Serializer/RestSerializer.php
@@ -244,6 +244,13 @@ abstract class RestSerializer
             $relative = substr($relative, 1);
         }
 
+        //Accounts for leading / in relative path
+        if ($this->api->isModifiedModel()
+            && strpos($relative, '//') === 0
+        ) {
+            return new Uri($this->endpoint . $relative);
+        }
+
         // Expand path place holders using Amazon's slightly different URI
         // template syntax.
         return UriResolver::resolve($this->endpoint, new Uri($relative));
