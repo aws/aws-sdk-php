214b8a3c7674bf13eef16f5f33098beb77c54352
Sean O'Brien <obrien.sean.dev@gmail.com>
2022-12-13 13:37:10 -0500

    undo s3 model change

--- src/S3/S3Client.php
+++ src/S3/S3Client.php
@@ -678,9 +678,6 @@ class S3Client extends AwsClient implements S3ClientInterface
         foreach($definition['operations'] as &$operation) {
             if (isset($operation['http']['requestUri'])) {
                 $requestUri = $operation['http']['requestUri'];
-//                $requestUri = str_replace('/{Bucket}', '/', $requestUri);
-//                $requestUri = str_replace('//', '/', $requestUri);
-//                $requestUri = str_replace('/?', '?', $requestUri);
                 if ($requestUri === "/{Bucket}") {
                     $requestUri = str_replace('/{Bucket}', '/', $requestUri);
                 } else {
