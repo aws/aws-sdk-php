1c0d5547e615fd5c5cf3dc6b981c414c7348dfc3
SamRemis <sjremis94@gmail.com>
2022-12-22 08:37:07 -0500

    Bugfix rollback (#2610)
    * Revert "bugfix: add MD5 header for PutObject and UploadPart by default"

--- tests/S3/ApplyChecksumMiddlewareTest.php
+++ tests/S3/ApplyChecksumMiddlewareTest.php
@@ -20,7 +20,8 @@ class ApplyChecksumMiddlewareTest extends TestCase
     public function testAddsContentMd5AsAppropriate($operation, $args, $md5Added, $md5Value)
     {
         $s3 = $this->getTestClient(
-            's3'
+            's3',
+            ['api_provider' => ApiProvider::filesystem(__DIR__ . '/fixtures')]
         );
         $this->addMockResults($s3, [[]]);
         $command = $s3->getCommand($operation, $args);
@@ -66,20 +67,6 @@ class ApplyChecksumMiddlewareTest extends TestCase
                 false,
                 null,
             ],
-            // Test MD5 added for operations which conditionally require it
-            [
-                'PutObject',
-                ['Bucket' => 'foo', 'Key'    => 'foo', 'Body' => 'test'],
-                true,
-                'CY9rzUYh03PK3k6DJie09g=='
-            ],
-            // Test MD5 added for operations which conditionally require it
-            [
-                'UploadPart',
-                ['Bucket' => 'foo', 'Key'    => 'foo', 'Body' => 'test', 'PartNumber' => 1, 'UploadId' => 1],
-                true,
-                'CY9rzUYh03PK3k6DJie09g=='
-            ]
         ];
     }
 
