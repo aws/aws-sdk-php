d81c87bf6c7527a6b8d679332a1b9937e235f721
Cintia <81837982+cintiashamsu@users.noreply.github.com>
2023-06-27 10:46:11 -0400

    Update MultipartUploaderTest.php

--- tests/S3/MultipartUploaderTest.php
+++ tests/S3/MultipartUploaderTest.php
@@ -356,7 +356,7 @@ class MultipartUploaderTest extends TestCase
             [
                 'bucket' => 'test-bucket',
                 'key' => 'test-key',
-                'track_upload' => 'true'
+                'track_upload' => true
             ]
         );
         $uploader->upload();
