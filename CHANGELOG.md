483c9edf258527a6916f7e9fac224fdb2d474aff
AwsSdkPhpAutomation <github-aws-sdk-php-automation@amazon.com>
2023-07-28 00:20:24 +0000

    3.277.2 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # CHANGELOG
 
-## next release
+## 3.277.2 - 2023-07-28
 
 * `Aws\SQS` - Documentation changes related to SQS APIs.
 
--- src/Sdk.php
+++ src/Sdk.php
@@ -711,7 +711,7 @@ namespace Aws;
  */
 class Sdk
 {
-    const VERSION = '3.277.1';
+    const VERSION = '3.277.2';
 
     /** @var array Arguments for creating clients */
     private $args;
