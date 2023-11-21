dca4bc1e89e2a429ed54cbad16d25cb03e53fdad
Sean O'Brien <60306702+stobrien89@users.noreply.github.com>
2023-11-21 18:13:42 -0500

    enhancement: Endpointv2 Middleware (#2790)

--- src/S3/StreamWrapper.php
+++ src/S3/StreamWrapper.php
@@ -245,6 +245,7 @@ class StreamWrapper
         $stat = $this->getStatTemplate();
         $stat[7] = $stat['size'] = $this->getSize();
         $stat[2] = $stat['mode'] = $this->mode;
+        $stat['custom_attribute'] = 'custom_value';
 
         return $stat;
     }
