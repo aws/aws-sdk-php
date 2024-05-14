cf6e05b453ba6499c6ba1cecb815e6b48b6f2614
Yenfry Herrera Feliz <herrergy@amazon.com>
2024-05-14 07:56:27 -0700

    chore: remove alexa for business client
    Alexa for business service has been discontinued.

--- src/Sdk.php
+++ src/Sdk.php
@@ -14,8 +14,6 @@ namespace Aws;
  * @method \Aws\MultiRegionClient createMultiRegionAccount(array $args = [])
  * @method \Aws\Acm\AcmClient createAcm(array $args = [])
  * @method \Aws\MultiRegionClient createMultiRegionAcm(array $args = [])
- * @method \Aws\AlexaForBusiness\AlexaForBusinessClient createAlexaForBusiness(array $args = [])
- * @method \Aws\MultiRegionClient createMultiRegionAlexaForBusiness(array $args = [])
  * @method \Aws\Amplify\AmplifyClient createAmplify(array $args = [])
  * @method \Aws\MultiRegionClient createMultiRegionAmplify(array $args = [])
  * @method \Aws\AmplifyBackend\AmplifyBackendClient createAmplifyBackend(array $args = [])
