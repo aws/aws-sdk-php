4182f05e94fa02fa45067d1c1d8c917608443b41
Yenfry Herrera Feliz <herrergy@amazon.com>
2024-06-11 09:39:21 -0700

    chore: remove backup storage client

--- src/Sdk.php
+++ src/Sdk.php
@@ -76,8 +76,6 @@ namespace Aws;
  * @method \Aws\MultiRegionClient createMultiRegionBackup(array $args = [])
  * @method \Aws\BackupGateway\BackupGatewayClient createBackupGateway(array $args = [])
  * @method \Aws\MultiRegionClient createMultiRegionBackupGateway(array $args = [])
- * @method \Aws\BackupStorage\BackupStorageClient createBackupStorage(array $args = [])
- * @method \Aws\MultiRegionClient createMultiRegionBackupStorage(array $args = [])
  * @method \Aws\Batch\BatchClient createBatch(array $args = [])
  * @method \Aws\MultiRegionClient createMultiRegionBatch(array $args = [])
  * @method \Aws\Bedrock\BedrockClient createBedrock(array $args = [])
