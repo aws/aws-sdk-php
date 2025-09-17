8e5541f283a20946b6d334b43ac3d6d4994d64b4
AwsSdkPhpAutomation <github-aws-sdk-php-automation@amazon.com>
2025-09-17 18:23:32 +0000

    3.356.20 release

--- tests/Exception/AwsExceptionTest.php
+++ tests/Exception/AwsExceptionTest.php
@@ -20,7 +20,7 @@ class AwsExceptionTest extends TestCase
 {
     use UsesServiceTrait;
 
-    public function testProvidesContextShortcuts()
+    public function testProvidesContextShortcuts(): void
     {
         $ctx = [
             'request_id' => '10',
@@ -37,7 +37,7 @@ class AwsExceptionTest extends TestCase
         $this->assertNull($e->getResult());
     }
 
-    public function testReturnsStatusCode()
+    public function testReturnsStatusCode(): void
     {
         $ctx = ['response' => new Response(400)];
         $command = new Command('foo');
@@ -45,7 +45,7 @@ class AwsExceptionTest extends TestCase
         $this->assertSame(400, $e->getStatusCode());
     }
 
-    public function testSetsMaxRetriesExceeded()
+    public function testSetsMaxRetriesExceeded(): void
     {
         $command = new Command('foo');
         $e = new AwsException('Foo', $command);
@@ -54,7 +54,7 @@ class AwsExceptionTest extends TestCase
         $this->assertTrue($e->isMaxRetriesExceeded());
     }
 
-    public function testProvidesResult()
+    public function testProvidesResult(): void
     {
         $command = new Command('foo');
         $result = new Result();
@@ -62,7 +62,7 @@ class AwsExceptionTest extends TestCase
         $this->assertSame($result, $e->getResult());
     }
 
-    public function testProvidesResponse()
+    public function testProvidesResponse(): void
     {
         $command = new Command('foo');
         $response = new Response();
@@ -70,28 +70,28 @@ class AwsExceptionTest extends TestCase
         $this->assertSame($response, $e->getResponse());
     }
 
-    public function testProvidesErrorMessage()
+    public function testProvidesErrorMessage(): void
     {
         $command = new Command('foo');
         $e = new AwsException('Foo', $command, ['message' => "test error message"]);
         $this->assertSame("test error message", $e->getAwsErrorMessage());
     }
 
-    public function testProvidesFalseConnectionErrorFlag()
+    public function testProvidesFalseConnectionErrorFlag(): void
     {
         $command = new Command('foo');
         $e = new AwsException('Foo', $command, ['connection_error' => false]);
         $this->assertFalse($e->isConnectionError());
     }
 
-    public function testProvidesTrueConnectionErrorFlag()
+    public function testProvidesTrueConnectionErrorFlag(): void
     {
         $command = new Command('foo');
         $e = new AwsException('Foo', $command, ['connection_error' => true]);
         $this->assertTrue($e->isConnectionError());
     }
 
-    public function testProvidesRequest()
+    public function testProvidesRequest(): void
     {
         $command = new Command('foo');
         $request = new Request('GET', 'http://www.foo.com');
@@ -99,7 +99,7 @@ class AwsExceptionTest extends TestCase
         $this->assertSame($request, $e->getRequest());
     }
 
-    public function testProvidesExceptionToStringWithNoPrevious()
+    public function testProvidesExceptionToStringWithNoPrevious(): void
     {
         $command = new Command('foo');
         $e = new AwsException('Foo', $command);
@@ -109,7 +109,7 @@ class AwsExceptionTest extends TestCase
         $this->assertStringStartsWith($exceptionString, $e->__toString());
     }
 
-    public function testProvidesExceptionToStringWithPreviousLast()
+    public function testProvidesExceptionToStringWithPreviousLast(): void
     {
         $prev = new \Exception("Last! '.");
         $command = new Command('foo');
@@ -120,7 +120,7 @@ class AwsExceptionTest extends TestCase
         );
     }
 
-    public function testHasData()
+    public function testHasData(): void
     {
         $e = new AwsException(
             'foo-message',
@@ -135,7 +135,7 @@ class AwsExceptionTest extends TestCase
         $this->assertSame('b', $e->search('a'));
     }
 
-    public function testProvidesErrorShape()
+    public function testProvidesErrorShape(): void
     {
         $command = new Command('foo');
         $response = new Response();
@@ -157,7 +157,7 @@ class AwsExceptionTest extends TestCase
         $this->assertSame($errorShape->toArray(), $e->getAwsErrorShape()->toArray());
     }
 
-    public function testCanIndirectlyModifyLikeAnArray()
+    public function testCanIndirectlyModifyLikeAnArray(): void
     {
         $e = new AwsException(
             'foo-message',
@@ -178,5 +178,85 @@ class AwsExceptionTest extends TestCase
         $q = $e['qux'];
         $q = 100;
         $this->assertSame(0, $e['qux']);
+    }
+
+    public function testAcceptsExceptionAsPrevious(): void
+    {
+        $previous = new \Exception('Previous exception');
+        $command = new Command('foo');
+        $e = new AwsException('Foo', $command, [], $previous);
+
+        $this->assertSame($previous, $e->getPrevious());
+        $this->assertInstanceOf(\Throwable::class, $e->getPrevious());
+    }
+
+    public function testAcceptsErrorAsPrevious(): void
+    {
+        $previous = new \Error('Previous error');
+        $command = new Command('foo');
+        $e = new AwsException('Foo', $command, [], $previous);
+
+        $this->assertSame($previous, $e->getPrevious());
+        $this->assertInstanceOf(\Throwable::class, $e->getPrevious());
+    }
+
+    public function testAcceptsCustomErrorAsPrevious(): void
+    {
+        $previous = new class extends \Error {
+            public function __construct() {
+                parent::__construct('Custom error');
+            }
+        };
+
+        $command = new Command('foo');
+        $e = new AwsException('Foo', $command, [], $previous);
+
+        $this->assertSame($previous, $e->getPrevious());
+        $this->assertInstanceOf(\Throwable::class, $e->getPrevious());
+    }
+
+    public function testAcceptsNullAsPrevious(): void
+    {
+        $command = new Command('foo');
+        $e = new AwsException('Foo', $command, [], null);
+
+        $this->assertNull($e->getPrevious());
+    }
+
+    /**
+     * @dataProvider previousThrowableProvider
+     */
+    public function testAcceptsVariousThrowableTypes(\Throwable $previous): void
+    {
+        $command = new Command('foo');
+        $e = new AwsException('Foo', $command, [], $previous);
+
+        $this->assertSame($previous, $e->getPrevious());
+        $this->assertInstanceOf(\Throwable::class, $e->getPrevious());
+    }
+
+    public function previousThrowableProvider(): array
+    {
+        return [
+            'standard exception' => [new \Exception('Standard exception')],
+            'runtime exception' => [new \RuntimeException('Runtime exception')],
+            'error' => [new \Error('Error')],
+            'type error' => [new \TypeError('Type error')],
+            'arithmetic error' => [new \ArithmeticError('Arithmetic error')],
+            'division by zero error' => [new \DivisionByZeroError('Division by zero')]
+        ];
+    }
+
+    public function testExceptionMessageWithErrorAsPrevious(): void
+    {
+        $previous = new \Error('Previous error');
+        $command = new Command('foo');
+        $e = new AwsException('Foo', $command, [], $previous);
+
+        $exceptionString = $e->__toString();
+
+        $this->assertStringContainsString('Foo', $exceptionString);
+        $this->assertStringContainsString('Previous error', $exceptionString);
+        $this->assertStringContainsString('Previous error', $e->__toString());
     }
 }
