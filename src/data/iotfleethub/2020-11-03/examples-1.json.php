beb4dd2666741675365f2121403498506db9252a
Sean O'Brien <60306702+stobrien89@users.noreply.github.com>
2025-11-07 14:06:44 -0500

    chore: remove deprecated services (#3207)

--- src/data/iotfleethub/2020-11-03/examples-1.json.php
+++
@@ -1,3 +0,0 @@
-<?php
-// This file was auto-generated from sdk-root/src/data/iotfleethub/2020-11-03/examples-1.json
-return [ 'version' => '1.0', 'examples' => [],];
--- src/data/robomaker/2018-06-29/examples-1.json.php
+++
@@ -1,3 +0,0 @@
-<?php
-// This file was auto-generated from sdk-root/src/data/robomaker/2018-06-29/examples-1.json
-return [ 'version' => '1.0', 'examples' => [],];
